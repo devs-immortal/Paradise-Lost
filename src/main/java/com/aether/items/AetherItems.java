78c3289a6ef74bd7069704c7dff0b5b19ca7b996
kalucky0 <kalucki23@gmail.com>
2020-10-28 21:39:14 +0100

    Add Gravitite armor logic

--- src/main/java/com/aether/items/AetherItems.java
+++ src/main/java/com/aether/items/AetherItems.java
@@ -7,6 +7,7 @@ import com.aether.items.tools.AetherPickaxeItem;
 import com.aether.items.tools.AetherShovelItem;
 import com.aether.items.tools.AetherSwordItem;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
@@ -22,6 +23,10 @@ public class AetherItems {
             new Identifier(Aether.MODID, "aether_tools"),
             () -> new ItemStack(AetherItems.GRAVITITE_PICKAXE));
 
+    public static final ItemGroup ARMOR_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(Aether.MODID, "aether_armor"),
+            () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE));
+
     public static final Item BLUE_PORTAL = new BlockItem(AetherBlocks.BLUE_PORTAL, new Item.Settings().group(null));
     public static final Item AETHER_DIRT = new BlockItem(AetherBlocks.AETHER_DIRT, new Item.Settings().group(BLOCKS_GROUP));
     public static final Item AETHER_GRASS = new BlockItem(AetherBlocks.AETHER_GRASS, new Item.Settings().group(BLOCKS_GROUP));
@@ -34,10 +39,18 @@ public class AetherItems {
     public static final Item HOLYSTONE_BRICK = new BlockItem(AetherBlocks.HOLYSTONE_BRICK, new Item.Settings().group(BLOCKS_GROUP));
     public static final Item MOSSY_HOLYSTONE = new BlockItem(AetherBlocks.MOSSY_HOLYSTONE, new Item.Settings().group(BLOCKS_GROUP));
 
+    // Don't worry, It's a placeholder. Ok?
+    public static final Item GRAVITITE_INGOT =  new Item((new Item.Settings()).group(null));
+
     public static final Item GRAVITITE_PICKAXE = new AetherPickaxeItem(ToolMaterials.DIAMOND, 1, -2.8F, (new Item.Settings()).group(TOOLS_GROUP));
     public static final Item GRAVITITE_AXE = new AetherAxeItem(ToolMaterials.DIAMOND, 5.0F, -3.0F, (new Item.Settings()).group(TOOLS_GROUP));
     public static final Item GRAVITITE_SHOVEL = new AetherShovelItem(ToolMaterials.DIAMOND, 1.5F, -3.0F, (new Item.Settings()).group(TOOLS_GROUP));
     public static final Item GRAVITITE_SWORD = new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, (new Item.Settings()).group(TOOLS_GROUP));
+
+    public static final Item GRAVITITE_BOOTS = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.FEET, (new Item.Settings()).group(ARMOR_GROUP));
+    public static final Item GRAVITITE_LEGGINGS = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.LEGS, (new Item.Settings()).group(ARMOR_GROUP));
+    public static final Item GRAVITITE_HELMET = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.HEAD, (new Item.Settings()).group(ARMOR_GROUP));
+    public static final Item GRAVITITE_CHESTPLATE = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.CHEST, (new Item.Settings()).group(ARMOR_GROUP));
 
     public static void initialization() {
         // Items (Blocks)
@@ -58,5 +71,13 @@ public class AetherItems {
         Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_axe"), GRAVITITE_AXE);
         Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_shovel"), GRAVITITE_SHOVEL);
         Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_sword"), GRAVITITE_SWORD);
+
+        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_ingot"), GRAVITITE_INGOT);
+
+        // Items (Armor)
+        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_boots"), GRAVITITE_BOOTS);
+        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_leggings"), GRAVITITE_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_helmet"), GRAVITITE_HELMET);
+        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_chestplate"), GRAVITITE_CHESTPLATE);
     }
 }
