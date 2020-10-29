4f56cac7a191e2bb61dc13dfcd0cf0be6213e7a8
Dragonoidzero <spectreneo@gmail.com>
2020-10-28 21:26:09 -0500

    Demistify feast
    Unfunkified a lot of stuff. Most notably I went ahead and streamlined block and item registration, unmurdered json, and started preparations to murder current portal code.

--- src/main/java/com/aether/blocks/AetherBlocks.java
+++ src/main/java/com/aether/blocks/AetherBlocks.java
@@ -3,15 +3,25 @@ package com.aether.blocks;
 import com.aether.Aether;
 import com.aether.blocks.natural.AetherGrassBlock;
 import com.aether.blocks.natural.EnchantedAetherGrassBlock;
+import com.aether.items.AetherItems;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
 
 public class AetherBlocks {
+
+    //private static AbstractBlock.Settings dirtMaterial() { return ; }
+
+    private static Item.Settings buildingBlock() { return new FabricItemSettings().group(AetherItems.BLOCKS_GROUP); }
+
     public static final Block BLUE_PORTAL = new PortalBlock(
             AbstractBlock.Settings.of(
                     Material.PORTAL, MaterialColor.BLUE)
@@ -23,32 +33,32 @@ public class AetherBlocks {
                     .strength(-1.0F)
                     .sounds(BlockSoundGroup.GLASS)
                     .luminance((state) -> 11));
-    public static final Block AETHER_DIRT = new Block(AbstractBlock.Settings.of(Material.SOIL, MaterialColor.DIRT).strength(0.3F).sounds(BlockSoundGroup.GRAVEL));
-    public static final Block AETHER_GRASS = new AetherGrassBlock(AbstractBlock.Settings.of(Material.SOLID_ORGANIC));
-    public static final Block AETHER_ENCHANTED_GRASS = new EnchantedAetherGrassBlock(AbstractBlock.Settings.of(Material.SOLID_ORGANIC));
 
-    public static final Block SKYROOT_LOG = createLogBlock(MaterialColor.WOOD, MaterialColor.SPRUCE);
-    public static final Block SKYROOT_PLANK = new Block(AbstractBlock.Settings.of(Material.WOOD, MaterialColor.WOOD).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD));
+    public static final Block AETHER_DIRT = register("aether_dirt", new Block(FabricBlockSettings.copy(Blocks.DIRT)), buildingBlock());
+    public static final Block AETHER_GRASS = register("aether_grass", new AetherGrassBlock(FabricBlockSettings.copyOf(Blocks.GRASS_BLOCK).materialColor(MaterialColor.CYAN)), buildingBlock());
+    public static final Block AETHER_ENCHANTED_GRASS = register("enchanted_aether_grass", new EnchantedAetherGrassBlock(FabricBlockSettings.copyOf(AETHER_GRASS).materialColor(MaterialColor.GOLD)), buildingBlock());
 
-    public static final Block HOLYSTONE = new Block(AbstractBlock.Settings.of(Material.STONE, MaterialColor.STONE).requiresTool().strength(1.5F, 6.0F));
-    public static final Block HOLYSTONE_BRICK = new Block(AbstractBlock.Settings.of(Material.STONE).requiresTool().strength(1.5F, 6.0F));
-    public static final Block MOSSY_HOLYSTONE = new Block(AbstractBlock.Settings.of(Material.STONE).requiresTool().strength(2.0F, 6.0F));
+    public static final Block SKYROOT_LOG = register("skyroot_log", createLogBlock(MaterialColor.WOOD, MaterialColor.GREEN), buildingBlock());
+    public static final Block SKYROOT_PLANK = register("skyroot_plank", new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS)), buildingBlock());
+
+    public static final Block HOLYSTONE = register("holystone", new Block(FabricBlockSettings.copyOf(Blocks.STONE).materialColor(MaterialColor.WHITE)), buildingBlock());
+    public static final Block HOLYSTONE_BRICK = register("holystone_brick", new Block(FabricBlockSettings.copyOf(Blocks.STONE_BRICKS).materialColor(MaterialColor.WHITE)), buildingBlock());
+    public static final Block MOSSY_HOLYSTONE = register("mossy_holystone", new Block(FabricBlockSettings.copyOf(Blocks.MOSSY_COBBLESTONE).materialColor(MaterialColor.WHITE)), buildingBlock());
 
     public static void initialization() {
-        // Blocks (Soil)
+        // NO-OP. But the game needs to be told this place exists
         Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "blue_portal"), BLUE_PORTAL);
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "aether_dirt"), AETHER_DIRT);
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "aether_grass"), AETHER_GRASS);
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "enchanted_aether_grass"), AETHER_ENCHANTED_GRASS);
+    }
 
-        // Blocks (Stone)
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "holystone"), HOLYSTONE);
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "holystone_brick"), HOLYSTONE_BRICK);
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "mossy_holystone"), MOSSY_HOLYSTONE);
+    private static Block register(String id, Block block, Item.Settings settings) {
+        Identifier trueId = new Identifier(Aether.MODID, id);
+        Registry.register(Registry.BLOCK, trueId, block);
+        Registry.register(Registry.ITEM, trueId, new BlockItem(block, settings));
+        return block;
+    }
 
-        // Blocks (Wood)
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "skyroot_log"), SKYROOT_LOG);
-        Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, "skyroot_plank"), SKYROOT_PLANK);
+    private static Block registerItemless(String id, Block block) {
+        return Registry.register(Registry.BLOCK, new Identifier(Aether.MODID, id), block);
     }
 
     private static PillarBlock createLogBlock(MaterialColor topMaterialColor, MaterialColor sideMaterialColor) {
--- src/main/java/com/aether/items/AetherItems.java
+++ src/main/java/com/aether/items/AetherItems.java
@@ -2,11 +2,9 @@ package com.aether.items;
 
 import com.aether.Aether;
 import com.aether.blocks.AetherBlocks;
-import com.aether.items.tools.AetherAxeItem;
-import com.aether.items.tools.AetherPickaxeItem;
-import com.aether.items.tools.AetherShovelItem;
-import com.aether.items.tools.AetherSwordItem;
+import com.aether.items.tools.*;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
@@ -23,61 +21,33 @@ public class AetherItems {
             new Identifier(Aether.MODID, "aether_tools"),
             () -> new ItemStack(AetherItems.GRAVITITE_PICKAXE));
 
+    private static Item.Settings defaultItem() { return new FabricItemSettings().group(TOOLS_GROUP); }
+
     public static final ItemGroup ARMOR_GROUP = FabricItemGroupBuilder.build(
             new Identifier(Aether.MODID, "aether_armor"),
             () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE));
 
     public static final Item BLUE_PORTAL = new BlockItem(AetherBlocks.BLUE_PORTAL, new Item.Settings().group(null));
-    public static final Item AETHER_DIRT = new BlockItem(AetherBlocks.AETHER_DIRT, new Item.Settings().group(BLOCKS_GROUP));
-    public static final Item AETHER_GRASS = new BlockItem(AetherBlocks.AETHER_GRASS, new Item.Settings().group(BLOCKS_GROUP));
-    public static final Item ENCHANTED_AETHER_GRASS = new BlockItem(AetherBlocks.AETHER_ENCHANTED_GRASS, new Item.Settings().group(BLOCKS_GROUP).rarity(Rarity.UNCOMMON));
 
-    public static final Item SKYROOT_LOG = new BlockItem(AetherBlocks.SKYROOT_LOG, new Item.Settings().group(BLOCKS_GROUP));
-    public static final Item SKYROOT_PLANK = new BlockItem(AetherBlocks.SKYROOT_PLANK, new Item.Settings().group(BLOCKS_GROUP));
+    public static final Item GRAVITITE_INGOT = register("gravitite_ingot", new Item(defaultItem()));
 
-    public static final Item HOLYSTONE = new BlockItem(AetherBlocks.HOLYSTONE, new Item.Settings().group(BLOCKS_GROUP));
-    public static final Item HOLYSTONE_BRICK = new BlockItem(AetherBlocks.HOLYSTONE_BRICK, new Item.Settings().group(BLOCKS_GROUP));
-    public static final Item MOSSY_HOLYSTONE = new BlockItem(AetherBlocks.MOSSY_HOLYSTONE, new Item.Settings().group(BLOCKS_GROUP));
+    public static final Item GRAVITITE_PICKAXE = register("gravitite_pickaxe", new AetherPickaxeItem(ToolMaterials.DIAMOND, 1, -2.8F, defaultItem()));
+    public static final Item GRAVITITE_AXE = register("gravitite_axe", new AetherAxeItem(ToolMaterials.DIAMOND, 5.0F, -3.0F, defaultItem()));
+    public static final Item GRAVITITE_SHOVEL = register("gravitite_shovel", new AetherShovelItem(ToolMaterials.DIAMOND, 1.5F, -3.0F, defaultItem()));
+    public static final Item GRAVITITE_SWORD = register("gravitite_sword", new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, defaultItem()));
+    public static final Item GRAVITITE_HOE = register("gravitite_hoe", new AetherHoeItem(ToolMaterials.DIAMOND, -1, 0.0F, defaultItem()));
 
-    // Don't worry, It's a placeholder. Ok?
-    public static final Item GRAVITITE_INGOT =  new Item((new Item.Settings()).group(null));
-
-    public static final Item GRAVITITE_PICKAXE = new AetherPickaxeItem(ToolMaterials.DIAMOND, 1, -2.8F, (new Item.Settings()).group(TOOLS_GROUP));
-    public static final Item GRAVITITE_AXE = new AetherAxeItem(ToolMaterials.DIAMOND, 5.0F, -3.0F, (new Item.Settings()).group(TOOLS_GROUP));
-    public static final Item GRAVITITE_SHOVEL = new AetherShovelItem(ToolMaterials.DIAMOND, 1.5F, -3.0F, (new Item.Settings()).group(TOOLS_GROUP));
-    public static final Item GRAVITITE_SWORD = new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, (new Item.Settings()).group(TOOLS_GROUP));
-
-    public static final Item GRAVITITE_BOOTS = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.FEET, (new Item.Settings()).group(ARMOR_GROUP));
-    public static final Item GRAVITITE_LEGGINGS = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.LEGS, (new Item.Settings()).group(ARMOR_GROUP));
-    public static final Item GRAVITITE_HELMET = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.HEAD, (new Item.Settings()).group(ARMOR_GROUP));
-    public static final Item GRAVITITE_CHESTPLATE = new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.CHEST, (new Item.Settings()).group(ARMOR_GROUP));
+    public static final Item GRAVITITE_BOOTS = register("gravitite_boots", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.FEET, (new Item.Settings()).group(ARMOR_GROUP)));
+    public static final Item GRAVITITE_LEGGINGS = register("gravitite_leggings", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.LEGS, (new Item.Settings()).group(ARMOR_GROUP)));
+    public static final Item GRAVITITE_HELMET = register("gravitite_helmet", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.HEAD, (new Item.Settings()).group(ARMOR_GROUP)));
+    public static final Item GRAVITITE_CHESTPLATE = register("gravitite_chestplate", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.CHEST, (new Item.Settings()).group(ARMOR_GROUP)));
 
     public static void initialization() {
-        // Items (Blocks)
+        // NO-OP. But the game needs to be told this place exists
         Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "blue_portal"), BLUE_PORTAL);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "aether_dirt"), AETHER_DIRT);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "aether_grass"), AETHER_GRASS);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "enchanted_aether_grass"), ENCHANTED_AETHER_GRASS);
+    }
 
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "skyroot_log"), SKYROOT_LOG);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "skyroot_plank"), SKYROOT_PLANK);
-
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "holystone"), HOLYSTONE);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "holystone_brick"), HOLYSTONE_BRICK);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "mossy_holystone"), MOSSY_HOLYSTONE);
-
-        // Items (Tools)
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_pickaxe"), GRAVITITE_PICKAXE);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_axe"), GRAVITITE_AXE);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_shovel"), GRAVITITE_SHOVEL);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_sword"), GRAVITITE_SWORD);
-
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_ingot"), GRAVITITE_INGOT);
-
-        // Items (Armor)
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_boots"), GRAVITITE_BOOTS);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_leggings"), GRAVITITE_LEGGINGS);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_helmet"), GRAVITITE_HELMET);
-        Registry.register(Registry.ITEM, new Identifier(Aether.MODID, "gravitite_chestplate"), GRAVITITE_CHESTPLATE);
+    private static Item register(String id, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(Aether.MODID, id), item);
     }
 }
