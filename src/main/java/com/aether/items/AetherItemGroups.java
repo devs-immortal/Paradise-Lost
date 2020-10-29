3220fd427d29276ef6274b9623d6e8f27e0ea73e
kalucky0 <kalucki23@gmail.com>
2020-10-29 09:57:33 +0100

    Add all item groups

--- src/main/java/com/aether/items/AetherItemGroups.java
+++ src/main/java/com/aether/items/AetherItemGroups.java
@@ -16,7 +16,27 @@ public class AetherItemGroups {
             new Identifier(Aether.MODID, "aether_tools"),
             () -> new ItemStack(AetherItems.GRAVITITE_PICKAXE));
 
+    public static final ItemGroup WEAPONS = FabricItemGroupBuilder.build(
+            new Identifier(Aether.MODID, "aether_weapons"),
+            () -> new ItemStack(AetherItems.GRAVITITE_SWORD));
+
     public static final ItemGroup ARMOR = FabricItemGroupBuilder.build(
             new Identifier(Aether.MODID, "aether_armor"),
             () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE));
+
+    public static final ItemGroup FOOD = FabricItemGroupBuilder.build(
+            new Identifier(Aether.MODID, "aether_food"),
+            () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE)); //TODO: Replace icon with Blue berry
+
+    public static final ItemGroup ACCESSORIES = FabricItemGroupBuilder.build(
+            new Identifier(Aether.MODID, "aether_accessories"),
+            () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE)); //TODO: Replace icon with Gravitite gloves
+
+    public static final ItemGroup MATERIALS = FabricItemGroupBuilder.build(
+            new Identifier(Aether.MODID, "aether_materials"),
+            () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE)); //TODO: Replace icon with Ambrosium shard
+
+    public static final ItemGroup MISC = FabricItemGroupBuilder.build(
+            new Identifier(Aether.MODID, "aether_misc"),
+            () -> new ItemStack(AetherItems.GRAVITITE_CHESTPLATE)); //TODO: Replace icon with Bronze key
 }
--- src/main/java/com/aether/items/AetherItems.java
+++ src/main/java/com/aether/items/AetherItems.java
@@ -1,18 +1,25 @@
 package com.aether.items;
 
 import com.aether.Aether;
-import com.aether.blocks.AetherBlocks;
 import com.aether.items.tools.*;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.*;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class AetherItems {
 
     private static Item.Settings defaultItem() {
+        return new FabricItemSettings().group(AetherItemGroups.TOOLS);
+    }
+
+    private static Item.Settings weaponItem() {
+        return new FabricItemSettings().group(AetherItemGroups.TOOLS);
+    }
+    private static Item.Settings armorItem() {
         return new FabricItemSettings().group(AetherItemGroups.TOOLS);
     }
 
@@ -56,7 +63,7 @@ public class AetherItems {
         GRAVITITE_PICKAXE = register("gravitite_pickaxe", new AetherPickaxeItem(AetherToolMaterials.GRAVITITE, 1, -2.8F, defaultItem()));
         GRAVITITE_AXE = register("gravitite_axe", new AetherAxeItem(AetherToolMaterials.GRAVITITE, 5.0F, -3.0F, defaultItem()));
         GRAVITITE_SHOVEL = register("gravitite_shovel", new AetherShovelItem(AetherToolMaterials.GRAVITITE, 1.5F, -3.0F, defaultItem()));
-        GRAVITITE_SWORD = register("gravitite_sword", new AetherSwordItem(AetherToolMaterials.GRAVITITE, 3, -2.4F, defaultItem()));
+        GRAVITITE_SWORD = register("gravitite_sword", new AetherSwordItem(AetherToolMaterials.GRAVITITE, 3, -2.4F, weaponItem()));
         GRAVITITE_HOE = register("gravitite_hoe", new AetherHoeItem(AetherToolMaterials.GRAVITITE, -1, 0.0F, defaultItem()));
 
         ZANITE_GEM = register("zanite_gemstone", new Item(defaultItem()));
@@ -64,25 +71,25 @@ public class AetherItems {
         ZANITE_PICKAXE = register("zanite_pickaxe", new AetherPickaxeItem(AetherToolMaterials.ZANITE, 1, -2.8F, defaultItem()));
         ZANITE_AXE = register("zanite_axe", new AetherAxeItem(AetherToolMaterials.ZANITE, 6.0F, -3.1F, defaultItem()));
         ZANITE_SHOVEL = register("zanite_shovel", new AetherShovelItem(AetherToolMaterials.ZANITE, 1.5F, -3.0F, defaultItem()));
-        ZANITE_SWORD = register("zanite_sword", new AetherSwordItem(AetherToolMaterials.ZANITE, 3, -2.4F, defaultItem()));
+        ZANITE_SWORD = register("zanite_sword", new AetherSwordItem(AetherToolMaterials.ZANITE, 3, -2.4F, weaponItem()));
         ZANITE_HOE = register("zanite_hoe", new AetherHoeItem(AetherToolMaterials.ZANITE, -2, -1.0F, defaultItem()));
 
         HOLYSTONE_PICKAXE = register("holystone_pickaxe", new AetherPickaxeItem(ToolMaterials.DIAMOND, 1, -2.8F, defaultItem()));
         HOLYSTONE_AXE = register("holystone_axe", new AetherAxeItem(ToolMaterials.DIAMOND, 5.0F, -3.0F, defaultItem()));
         HOLYSTONE_SHOVEL = register("holystone_shovel", new AetherShovelItem(ToolMaterials.DIAMOND, 1.5F, -3.0F, defaultItem()));
-        HOLYSTONE_SWORD = register("holystone_sword", new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, defaultItem()));
+        HOLYSTONE_SWORD = register("holystone_sword", new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, weaponItem()));
         HOLYSTONE_HOE = register("holystone_hoe", new AetherHoeItem(ToolMaterials.DIAMOND, -1, 0.0F, defaultItem()));
 
         SKYROOT_PICKAXE = register("skyroot_pickaxe", new AetherPickaxeItem(ToolMaterials.DIAMOND, 1, -2.8F, defaultItem()));
         SKYROOT_AXE = register("skyroot_axe", new AetherAxeItem(ToolMaterials.DIAMOND, 5.0F, -3.0F, defaultItem()));
         SKYROOT_SHOVEL = register("skyroot_shovel", new AetherShovelItem(ToolMaterials.DIAMOND, 1.5F, -3.0F, defaultItem()));
-        SKYROOT_SWORD = register("skyroot_sword", new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, defaultItem()));
+        SKYROOT_SWORD = register("skyroot_sword", new AetherSwordItem(ToolMaterials.DIAMOND, 3, -2.4F, weaponItem()));
         SKYROOT_HOE = register("skyroot_hoe", new AetherHoeItem(ToolMaterials.DIAMOND, -1, 0.0F, defaultItem()));
 
-        GRAVITITE_BOOTS = register("gravitite_boots", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.FEET, (new Item.Settings()).group(AetherItemGroups.ARMOR)));
-        GRAVITITE_LEGGINGS = register("gravitite_leggings", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.LEGS, (new Item.Settings()).group(AetherItemGroups.ARMOR)));
-        GRAVITITE_HELMET = register("gravitite_helmet", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.HEAD, (new Item.Settings()).group(AetherItemGroups.ARMOR)));
-        GRAVITITE_CHESTPLATE = register("gravitite_chestplate", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.CHEST, (new Item.Settings()).group(AetherItemGroups.ARMOR)));
+        GRAVITITE_BOOTS = register("gravitite_boots", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.FEET, armorItem()));
+        GRAVITITE_LEGGINGS = register("gravitite_leggings", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.LEGS, armorItem()));
+        GRAVITITE_HELMET = register("gravitite_helmet", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.HEAD, armorItem()));
+        GRAVITITE_CHESTPLATE = register("gravitite_chestplate", new ArmorItem(AetherArmorMaterials.GRAVITITE, EquipmentSlot.CHEST, armorItem()));
     }
 
     private static Item register(String id, Item item) {
