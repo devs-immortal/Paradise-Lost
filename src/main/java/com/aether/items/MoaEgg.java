41f488eb16fd799e862d113a046ba82e0e6487a1
kalucky0 <kalucki23@gmail.com>
2020-10-30 23:40:59 +0100

    Add MoaEgg

---
+++ src/main/java/com/aether/items/MoaEgg.java
@@ -0,0 +1,28 @@
+package com.aether.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+
+public class MoaEgg extends Item {
+
+    public MoaEgg() {
+        super(new Item.Settings().maxCount(1).group(AetherItemGroups.MISC));
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext contextIn) {
+        if (contextIn.getPlayer().abilities.creativeMode) {
+
+            //TODO: Spawn Moa
+
+            return ActionResult.SUCCESS;
+        }
+        return super.useOnBlock(contextIn);
+    }
+
+    @Override
+    public boolean shouldSyncTagToClient() {
+        return true;
+    }
+}
