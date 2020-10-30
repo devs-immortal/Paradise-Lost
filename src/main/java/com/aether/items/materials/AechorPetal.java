6204f631ffa416518a9c1cb7c0759c132396467d
kalucky0 <kalucki23@gmail.com>
2020-10-30 23:28:20 +0100

    Add materials classes

---
+++ src/main/java/com/aether/items/materials/AechorPetal.java
@@ -0,0 +1,10 @@
+package com.aether.items.materials;
+
+import net.minecraft.item.Item;
+import com.aether.items.AetherItemGroups;
+
+public class AechorPetal extends Item {
+    public AechorPetal() {
+        super(new Settings().group(AetherItemGroups.MATERIALS));
+    }
+}
---
+++ src/main/java/com/aether/items/materials/ZaniteGemstone.java
@@ -0,0 +1,10 @@
+package com.aether.items.materials;
+
+import net.minecraft.item.Item;
+import com.aether.items.AetherItemGroups;
+
+public class ZaniteGemstone extends Item {
+    public ZaniteGemstone() {
+        super(new Settings().group(AetherItemGroups.MATERIALS));
+    }
+}
