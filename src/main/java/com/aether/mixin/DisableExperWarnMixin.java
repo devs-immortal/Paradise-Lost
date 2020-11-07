ece0807aef07657dbf5d5956bbd421f200f127a5
CyaCal <calumproh28@gmail.com>
2020-11-07 17:29:22 +0000

    Added mixin to disable warning when using mod.

---
+++ src/main/java/com/aether/mixin/DisableExperWarnMixin.java
@@ -0,0 +1,15 @@
+package com.aether.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(MinecraftClient.class)
+public abstract class DisableExperWarnMixin {
+    // TODO: Remember to change this in 1.17
+    @ModifyVariable(method = "startIntegratedServer(Ljava/lang/String;Lnet/minecraft/util/registry/DynamicRegistryManager$Impl;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient$WorldLoadAction;NONE:Lnet/minecraft/client/MinecraftClient$WorldLoadAction;", ordinal = 0), ordinal = 2,index = 11,name = "bl2",require = 1)
+    private boolean replaceBl2(boolean bl2) {
+        return false;
+    }
+}
