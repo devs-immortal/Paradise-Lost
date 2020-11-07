2c7b67aa18436d047a061744aee0d9fe9c95e726
CDAGaming <cstack2011@yahoo.com>
2020-11-06 23:38:46 -0600

    [Fix] Fixed Issue #7; Farmland will now revert to aether's dirt under co

---
+++ src/main/java/com/aether/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,24 @@
+package com.aether.mixin;
+
+import com.aether.blocks.AetherBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin {
+    @Inject(method = "setToDirt", at = @At("HEAD"), cancellable = true)
+    private static void onSetToDirt(BlockState state, World world, BlockPos pos, CallbackInfo ci) {
+        if (state.isOf(AetherBlocks.AETHER_FARMLAND)) {
+            world.setBlockState(pos, Block.pushEntitiesUpBeforeBlockChange(state, AetherBlocks.AETHER_DIRT.getDefaultState(), world, pos));
+            ci.cancel();
+        }
+    }
+}
