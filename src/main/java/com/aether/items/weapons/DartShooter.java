c2bf6d01e145feaf7533b8b665f0b1f4c9611657
kalucky0 <kalucki23@gmail.com>
2020-11-11 17:36:32 +0100

    Fix #19 and #18

--- src/main/java/com/aether/items/weapons/DartShooter.java
+++ src/main/java/com/aether/items/weapons/DartShooter.java
@@ -4,9 +4,11 @@ import com.aether.items.AetherItemGroups;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.TypedActionResult;
@@ -38,20 +40,23 @@ public class DartShooter extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack heldItem = playerIn.getStackInHand(handIn);
-        boolean bypassDartCheck = playerIn.abilities.creativeMode || EnchantmentHelper.getLevel(Enchantments.INFINITY, heldItem) > 0;
+        boolean bypassDartCheck = playerIn.isCreative() || EnchantmentHelper.getLevel(Enchantments.INFINITY, heldItem) > 0;
 
         ItemStack stack = this.findDartStack(playerIn);
-
         if (!stack.isEmpty() || bypassDartCheck) {
-            if (stack.isEmpty()) {
-                stack = new ItemStack(this.ammo);
-            }
+            if (stack.isEmpty()) stack = new ItemStack(this.ammo);
 
-            ProjectileEntity projectile = this.ammo.createDart(worldIn, heldItem, playerIn);
+            PersistentProjectileEntity projectile = this.ammo.createDart(worldIn, heldItem, playerIn);
 
             if (!worldIn.isClient) {
                 projectile.setProperties(playerIn, playerIn.pitch, playerIn.yaw, 0.0F, 1.0F, 1.0F);
                 worldIn.spawnEntity(projectile);
+
+                if (!playerIn.isCreative()) {
+                    projectile.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
+                    stack.decrement(1);
+                }
+                else if (playerIn.isCreative()) projectile.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
             }
 
 //            worldIn.playSound(playerIn, playerIn.getBlockPos(), AetherSounds.DART_SHOOTER_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (playerIn.getRandom().nextFloat() * 0.4F + 0.8F));
@@ -59,4 +64,5 @@ public class DartShooter extends Item {
 
         return super.use(worldIn, playerIn, handIn);
     }
+
 }
