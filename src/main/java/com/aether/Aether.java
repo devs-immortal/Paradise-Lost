5102b8653de4b5b7e302e9711e00a048f2a0a90e
kyrptonaught <anthonybuttillo10@gmail.com>
2020-11-11 15:02:46 -0500

    Implement CustomPortalApi

--- src/main/java/com/aether/Aether.java
+++ src/main/java/com/aether/Aether.java
@@ -7,6 +7,7 @@ import com.aether.world.dimension.AetherDimension;
 import de.guntram.mcmod.crowdintranslate.CrowdinTranslate;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.kyrptonaught.customportalapi.CustomPortalApiRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
@@ -22,11 +23,6 @@ public class Aether implements ModInitializer, ClientModInitializer {
     public static final String modId = "the_aether";
     public static final Identifier MOD_DIMENSION_ID = new Identifier(Aether.modId, Aether.modId);
     public static final Logger modLogger = LogManager.getLogger(modId);
-    public static final Set<Block> PORTAL_BLOCKS = new HashSet<>();
-
-    static {
-        PORTAL_BLOCKS.add(Blocks.GLOWSTONE);
-    }
 
     public static Identifier locate(String location) {
         return new Identifier(modId, location);
--- src/main/java/com/aether/world/dimension/AetherDimension.java
+++ src/main/java/com/aether/world/dimension/AetherDimension.java
@@ -1,8 +1,13 @@
 package com.aether.world.dimension;
 
 import com.aether.Aether;
+import com.aether.blocks.AetherBlocks;
 import com.aether.world.biome.AetherBiomeProvider;
 import com.aether.world.surfacebuilders.AetherSurfaceBuilders;
+import net.kyrptonaught.customportalapi.CustomPortalApiRegistry;
+import net.kyrptonaught.customportalapi.CustomPortalBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
@@ -15,5 +20,7 @@ public class AetherDimension {
         AetherBiomeProvider.registerBiomeProvider();
 
         AetherSurfaceBuilders.registerSurfaceBuilders();
+
+        CustomPortalApiRegistry.addPortal(Blocks.GLOWSTONE, Blocks.WATER, (CustomPortalBlock)AetherBlocks.BLUE_PORTAL, Aether.MOD_DIMENSION_ID, DyeColor.LIGHT_BLUE.getMaterialColor().color);
     }
 }
