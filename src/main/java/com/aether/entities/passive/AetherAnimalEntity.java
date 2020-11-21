f85a5b0532e43136e8e5a445ca402e30cb8623c2
CDAGaming <cstack2011@yahoo.com>
2020-11-20 19:31:53 -0600

    [Change] Ensure Parity with Aether-Recoded AnimalEntity
    Notable Changes:
     - Pathfinding favoring and breeding adjusted to properly represent data

--- src/main/java/com/aether/entities/passive/AetherAnimalEntity.java
+++ src/main/java/com/aether/entities/passive/AetherAnimalEntity.java
@@ -1,18 +1,31 @@
 package com.aether.entities.passive;
 
+import com.aether.blocks.AetherBlocks;
+import com.aether.items.AetherItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
 public class AetherAnimalEntity extends AnimalEntity {
 
     protected AetherAnimalEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
-        // TODO: ???
-        // this.spawningGround = AetherBlocks.aether_grass;
+    }
+
+    @Override
+    public float getPathfindingFavor(BlockPos pos, WorldView worldIn) {
+        return worldIn.getBlockState(pos.down()).getBlock() == AetherBlocks.AETHER_GRASS ? 10.0F : worldIn.getLightLevel(pos) - 0.5F;
+    }
+
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return stack.getItem() == AetherItems.BLUE_BERRY;
     }
 
     @Override
