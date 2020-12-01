0c2fe24629e209d516c2d733f49b68c12665950c
Haven <haven@hephaestus.dev>
2020-11-30 19:57:16 -0800

    - Reimplements a custom surface builder for later use. - Adds DataBreaker for faster worldgen in a dev environment.

--- src/main/java/com/aether/world/dimension/AetherDimension.java
+++ src/main/java/com/aether/world/dimension/AetherDimension.java
@@ -2,6 +2,8 @@ package com.aether.world.dimension;
 
 import com.aether.Aether;
 import com.aether.blocks.AetherBlocks;
+import com.aether.world.gen.AetherSurfaceBuilder;
+import com.aether.world.gen.AetherSurfaceBuilderConfig;
 import net.kyrptonaught.customportalapi.CustomPortalApiRegistry;
 import net.kyrptonaught.customportalapi.CustomPortalBlock;
 import net.minecraft.block.Blocks;
@@ -9,9 +11,13 @@ import net.minecraft.util.DyeColor;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 
 public class AetherDimension {
     public static final RegistryKey<World> AETHER_WORLD_KEY = RegistryKey.of(Registry.DIMENSION, Aether.locate(Aether.MOD_ID));
+
+    public static final SurfaceBuilder<AetherSurfaceBuilderConfig> AETHER_SURFACE_BUILDER =
+            Registry.register(Registry.SURFACE_BUILDER, Aether.locate("surface_builder"), new AetherSurfaceBuilder());
 
     public static void setupDimension() {
         CustomPortalApiRegistry.addPortal(Blocks.GLOWSTONE, Blocks.WATER, (CustomPortalBlock) AetherBlocks.BLUE_PORTAL, Aether.locate(Aether.MOD_ID), DyeColor.LIGHT_BLUE.getMaterialColor().color);
