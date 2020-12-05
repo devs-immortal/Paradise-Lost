6d844def8475c3c36b5b8d4d01f0f13482e748ad
kyrptonaught <anthonybuttillo10@gmail.com>
2020-12-05 18:46:21 -0500

    Update CustomPortalApi (#45)
    Updates your included version of CustomPortalApi, with support for the new syntax.
    Allows skyroot bucket to light the portal.
    Removes unneded old code for old portal implementation

--- src/main/java/com/aether/world/dimension/AetherDimension.java
+++ src/main/java/com/aether/world/dimension/AetherDimension.java
@@ -4,6 +4,7 @@ import com.aether.Aether;
 import com.aether.blocks.AetherBlocks;
 import net.kyrptonaught.customportalapi.CustomPortalApiRegistry;
 import net.kyrptonaught.customportalapi.CustomPortalBlock;
+import net.kyrptonaught.customportalapi.portal.PortalIgnitionSource;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.registry.Registry;
@@ -14,6 +15,6 @@ public class AetherDimension {
     public static final RegistryKey<World> AETHER_WORLD_KEY = RegistryKey.of(Registry.DIMENSION, Aether.locate(Aether.MOD_ID));
 
     public static void setupDimension() {
-        CustomPortalApiRegistry.addPortal(Blocks.GLOWSTONE, Blocks.WATER, (CustomPortalBlock) AetherBlocks.BLUE_PORTAL, Aether.locate(Aether.MOD_ID), DyeColor.LIGHT_BLUE.getMaterialColor().color);
+        CustomPortalApiRegistry.addPortal(Blocks.GLOWSTONE, PortalIgnitionSource.WATER, (CustomPortalBlock) AetherBlocks.BLUE_PORTAL, Aether.locate(Aether.MOD_ID), 55,89,195);
     }
 }
