65480828bc0e4cd51ede6f1ae2b4a19c8ee26613
Haven <haven@hephaestus.dev>
2020-12-13 16:18:06 -0800

    - Establishes better practices for block definition

--- src/main/java/com/aether/blocks/natural/EnchantedAetherGrassBlock.java
+++ src/main/java/com/aether/blocks/natural/EnchantedAetherGrassBlock.java
@@ -1,18 +1,7 @@
 package com.aether.blocks.natural;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-import java.util.Random;
-
 public class EnchantedAetherGrassBlock extends AetherGrassBlock {
-    public EnchantedAetherGrassBlock(Settings of) {
-        super(of);
-    }
-
-    @Override
-    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        super.randomDisplayTick(state, world, pos, random);
+    public EnchantedAetherGrassBlock(Settings settings) {
+        super(settings);
     }
 }
