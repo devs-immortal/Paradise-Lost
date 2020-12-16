78408b0ef61449963354d6c6b7ff0895274d4006
CyaCal <calumproh28@gmail.com>
2020-12-16 14:28:10 +0000

    Temporarily Disable API

--- src/main/java/com/aether/api/moa/MoaProperties.java
+++ src/main/java/com/aether/api/moa/MoaProperties.java
@@ -1,31 +1,31 @@
-package com.aether.api.moa;
-
-import net.minecraft.util.Identifier;
-
-public class MoaProperties {
-
-    private final int maxJumps;
-    private final float moaSpeed;
-    private final Identifier location;
-    private final Identifier saddleLocation;
-
-    public MoaProperties(int maxJumps, float moaSpeed, Identifier location, Identifier saddleLocation) {
-        this.maxJumps = maxJumps;
-        this.moaSpeed = moaSpeed;
-        this.location = location;
-        this.saddleLocation = saddleLocation;
-    }
-
-    public int getMaxJumps() {
-        return this.maxJumps;
-    }
-
-    public float getMoaSpeed() {
-        return this.moaSpeed;
-    }
-
-    public Identifier getCustomTexture(boolean isSaddled) {
-        return isSaddled ? this.saddleLocation : this.location;
-    }
-
-}
+//package com.aether.api.moa;
+//
+//import net.minecraft.util.Identifier;
+//
+//public class MoaProperties {
+//
+//    private final int maxJumps;
+//    private final float moaSpeed;
+//    private final Identifier location;
+//    private final Identifier saddleLocation;
+//
+//    public MoaProperties(int maxJumps, float moaSpeed, Identifier location, Identifier saddleLocation) {
+//        this.maxJumps = maxJumps;
+//        this.moaSpeed = moaSpeed;
+//        this.location = location;
+//        this.saddleLocation = saddleLocation;
+//    }
+//
+//    public int getMaxJumps() {
+//        return this.maxJumps;
+//    }
+//
+//    public float getMoaSpeed() {
+//        return this.moaSpeed;
+//    }
+//
+//    public Identifier getCustomTexture(boolean isSaddled) {
+//        return isSaddled ? this.saddleLocation : this.location;
+//    }
+//
+//}
