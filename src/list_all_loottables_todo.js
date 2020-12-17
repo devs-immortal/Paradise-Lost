c6c7f7ab8972e58e2ca553ad70678a0f4716a231
CyaCal <calumproh28@gmail.com>
2020-12-17 20:37:21 +0000

    Added loot tables. Part 1

---
+++ src/list_all_loottables_todo.js
@@ -0,0 +1,29 @@
+const resourcesFolder = './main/resources/assets/the_aether/models/block';
+const lootTableFolder = './main/resources/data/the_aether/loot_tables/blocks'
+const fs = require('fs');
+
+models = [];
+alreadyDoneLoottables = [];
+
+async function run() {
+    fs.readdir(resourcesFolder, (err, files) => {
+      if(err) throw err;
+      files.forEach(file => {
+        models.push(file.replace(".json", ""))
+      });
+      fs.readdir(lootTableFolder, (err, files) => {
+              if(err) throw err;
+              files.forEach(file => {
+                  alreadyDoneLoottables.push(file.replace(".json", ""));
+              });
+              const intersection = models.filter(element => !alreadyDoneLoottables.includes(element));
+              let data = "# Loot Tables TODO:\n\n";
+              intersection.forEach(insect => {
+                data += `- [ ] ${insect}\n`
+              });
+              fs.writeFileSync("./todo.md", data);
+          });
+    });
+}
+
+run();
---
+++ src/main/java/com/aether/executable/InstallerForm.java
@@ -0,0 +1,4 @@
+package com.aether.executable;
+
+public class InstallerForm {
+}
