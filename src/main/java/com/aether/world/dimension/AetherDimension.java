ad6605eda6944ef09af0ecd14c6511359adbec13
kyrptonaught <anthonybuttillo10@gmail.com>
2020-12-20 15:30:32 -0500

    Add forced portal size of 2x3

--- src/main/java/com/aether/world/dimension/AetherDimension.java
+++ src/main/java/com/aether/world/dimension/AetherDimension.java
@@ -5,6 +5,7 @@ import com.aether.blocks.AetherBlocks;
 import net.kyrptonaught.customportalapi.CustomPortalApiRegistry;
 import net.kyrptonaught.customportalapi.CustomPortalBlock;
 import net.kyrptonaught.customportalapi.portal.PortalIgnitionSource;
+import net.kyrptonaught.customportalapi.util.PortalLink;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.registry.Registry;
@@ -15,6 +16,6 @@ public class AetherDimension {
     public static final RegistryKey<World> AETHER_WORLD_KEY = RegistryKey.of(Registry.DIMENSION, Aether.locate(Aether.MOD_ID));
 
     public static void setupDimension() {
-        CustomPortalApiRegistry.addPortal(Blocks.GLOWSTONE, PortalIgnitionSource.WATER, (CustomPortalBlock) AetherBlocks.BLUE_PORTAL, Aether.locate(Aether.MOD_ID), 55,89,195);
+        CustomPortalApiRegistry.addPortal(Blocks.GLOWSTONE, PortalIgnitionSource.WATER, (CustomPortalBlock) AetherBlocks.BLUE_PORTAL, Aether.locate(Aether.MOD_ID), 2, 3, 55, 89, 195);
     }
 }
