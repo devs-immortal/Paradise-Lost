a476e377f32cfd16ea9f485999c5d00ae00cb818
24Chrome <m.batt1@outlook.com>
2021-01-31 15:23:43 -0600

    Phoenix Armor model + texture

--- src/main/java/com/aether/client/model/armor/PhoenixArmorModel.java
+++ src/main/java/com/aether/client/model/armor/PhoenixArmorModel.java
@@ -9,16 +9,16 @@ public class PhoenixArmorModel extends AnimatedGeoModel<AetherArmor> {
 
     @Override
     public Identifier getModelLocation(AetherArmor object) {
-        return Aether.locate("geo/phoenix_armor.geo.json");
+        return Aether.locate("geo/phoenix_armor/phoenix_armor.geo.json");
     }
 
     @Override
     public Identifier getTextureLocation(AetherArmor object) {
-        return Aether.locate("textures/armor/phoenix_armor.png");
+        return Aether.locate("textures/models/armor/phoenix_armor.png");
     }
 
     @Override
     public Identifier getAnimationFileLocation(AetherArmor animatable) {
-        return Aether.locate("animations/armor/phoenix_armor.animation");
+        return Aether.locate("animations/phoenix_armor/phoenix_armor.animation");
     }
 }
