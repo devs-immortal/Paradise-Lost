16119acfd2aba55d2c70c63b5f11e1c162d29012
24Chrome <m.batt1@outlook.com>
2021-03-28 18:02:29 -0500

    Recipe redo part 2
    * Misc block recipes
    * leaf piles
    * smelting ores
    * torches
    * mossy and brick holystone
    * zanite block to ingot and back

--- src/main/resources/data/the_aether/recipes/block_populator.py
+++ src/main/resources/data/the_aether/recipes/block_populator.py
@@ -229,6 +229,32 @@ def pop_block_set(parent_id, fence_bool, wall_bool, cracked_bool):
         cracked_file.close()
     
     
+    
+def pop_tool_set(material_name, mod_id):
+    sword_file = open(material_name + "_sword.json", "x")
+    pickaxe_file = open(material_name + "_pickaxe.json", "x")
+    axe_file = open(material_name + "_axe.json", "x")
+    shovel_file = open(material_name + "_shovel.json", "x")
+    hoe_file = open(material_name + "_hoe.json", "x")
+    
+    sword_file.write(
+    """
+
+    """    
+    )
+    pickaxe_file.write(
+    """
+
+    """    
+    )
+    
+    sword_file.close()
+    pickaxe_file.close()
+    axe_file.close()
+    shovel_file.close()
+    hoe_file.close()
+    
+
 while True:
     selection = input("\n\n What do you want to add?\n  0.) Quit\n  1.) Populate a block set\n ")
     if selection == "0":
@@ -242,47 +268,3 @@ while True:
         os.system('cls')
     else:
         print(" Enter valid selection!")
-  
-
-"""
-def print_block_commands(block_id, name, logic_bool, model_num):
-    print("\n block_world_logic text:\n# "+block_id.upper()+"\nexecute as @e[tag=spawn_"+block_id+"] at @s run function tellurian:blocks/"+block_id+"/place\nexecute as @e[tag="+block_id+"] at @s if block ~ ~ ~ air run function tellurian:blocks/"+block_id+"/break")
-    if logic_bool:
-        print("execute as @e[tag="+block_id+"] at @s run function tellurian:blocks/"+block_id+"/logic")
-    setup_block_files(block_id, name, logic_bool,model_num)
-
-def setup_block_files(block_id, name, logic_bool, model_num):
-    block_directory = directory+"//blocks//"+block_id
-    give_file = open(directory+"//give_stuff.mcfunction","a")
-    os.mkdir(block_directory)
-    
-    break_file = open(block_directory+"//break.mcfunction", "x")
-    break_file.write('execute if block ~ ~ ~ air run particle minecraft:item dolphin_spawn_egg{CustomModelData:'+model_num+'} ~ ~.5 ~ 0.3 0.3 0.3 0.05 40\nexecute if block ~ ~ ~ air unless entity @p[gamemode=creative] run summon item ~ ~ ~ {Motion:[0.0,0.25,0.0],Item:{id:"minecraft:dolphin_spawn_egg",Count:1b,tag:{CustomModelData:'+model_num+',display:{Name:"[{\\"text\\":\\"'+name+'\\",\\"italic\\":\\"false\\"}]"},EntityTag:{id:"minecraft:area_effect_cloud",Tags:["ent_spawn","spawn_'+block_id+'"],Duration:0,Radius:0f,Particle:"minecraft:dust 0 0 0 0"}}}}\nexecute if block ~ ~ ~ air run kill @s')
-    break_file.close()
-    
-    place_file = open(block_directory+"//place.mcfunction", "x")
-    place_file.write('execute unless block ~ ~ ~ air unless entity @p[gamemode=creative] run summon item ~ ~ ~ {Motion:[0.0,0.25,0.0],Item:{id:"minecraft:dolphin_spawn_egg",Count:1b,tag:{CustomModelData:'+model_num+',display:{Name:"[{\\"text\\":\\"'+name+'\\",\\"italic\\":\\"false\\"}]"},EntityTag:{id:"minecraft:area_effect_cloud",Tags:["ent_spawn","spawn_'+block_id+'"],Duration:0,Radius:0f,Particle:"minecraft:dust 0 0 0 0"}}}}\nexecute if block ~ ~ ~ air run summon minecraft:item_frame ~ ~ ~ {Tags:["'+block_id+'"],Fixed:1b,Facing:1b,Item:{id:"minecraft:dolphin_spawn_egg", Count:1b, tag:{CustomModelData:'+model_num+'}}}\nexecute if block ~ ~ ~ air run setblock ~ ~ ~ glass')
-    place_file.close()
-    
-    if logic_bool:
-        logic_file = open(block_directory+"//logic.mcfunction", "x")
-        logic_file.close()  
-        
-    give_file.write('\ngive @p minecraft:dolphin_spawn_egg{CustomModelData:'+model_num+',display:{Name:"[{\\"text\\":\\"'+name+'\\",\\"italic\\":\\"false\\"}]"},EntityTag:{id:"minecraft:area_effect_cloud",Tags:["ent_spawn","spawn_'+block_id+'"],Duration:0,Radius:0f,Particle:"minecraft:dust 0 0 0 0"}}')
-    give_file.close()
-
-selection = ""
-while True:
-    selection = input("\n\n What do you want to do?\n  0.) Quit\n  1.) Add new block\n ")
-    if selection == "0":
-        break
-    elif selection == "1":
-        id = input("  What is the block's id?\n ").lower()
-        name = input("  What is the block's actual name?\n ")
-        logic = input("  Does the block have logic? (y/n)\n ").lower()
-        model_num = input("  What's the block's model value?\n ").lower()
-        logic_bool = (logic == "y")
-        print_block_commands(id, name, logic_bool,model_num)
-    else:
-        print(" Enter valid selection!")
-"""
