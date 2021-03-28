6fe2aedad8ef36376b93a40b80a6202a2b9b0604
CDAGaming <cstack2011@yahoo.com>
2021-03-28 01:04:11 -0500

    Maintainer Update - 1.16.5 official support

--- src/main/java/com/aether/util/RegistryUtil.java
+++ src/main/java/com/aether/util/RegistryUtil.java
@@ -1,13 +1,13 @@
 package com.aether.util;
 
-import net.minecraft.class_5423;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.RegistryWorldView;
 import net.minecraft.world.dimension.DimensionType;
 import org.jetbrains.annotations.Nullable;
 
 public class RegistryUtil {
-	public static boolean dimensionMatches(@Nullable class_5423 world, RegistryKey<DimensionType> type) {
+	public static boolean dimensionMatches(@Nullable RegistryWorldView world, RegistryKey<DimensionType> type) {
 		if (world == null) return false;
 
 		DimensionType shatteredSky = world.getRegistryManager().get(Registry.DIMENSION_TYPE_KEY).get(type);
