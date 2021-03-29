c5e0f541145e3fc6a01cfae87e886e07458ac9d4
24Chrome <m.batt1@outlook.com>
2021-03-28 19:31:27 -0500

    Recipe redo part 3
    * Tool and armor sets for gravitite and zanite

--- src/main/resources/data/the_aether/recipes/block_populator.py
+++ src/main/resources/data/the_aether/recipes/block_populator.py
@@ -1,5 +1,5 @@
 import os
-
+mod_id = "the_aether"
   
 def pop_block_set(parent_id, fence_bool, wall_bool, cracked_bool):
     stair_file = open(parent_id + "_stairs.json", "x")
@@ -229,8 +229,7 @@ def pop_block_set(parent_id, fence_bool, wall_bool, cracked_bool):
         cracked_file.close()
     
     
-    
-def pop_tool_set(material_name, mod_id):
+def pop_tool_set(material_name, material_id, mod_id):
     sword_file = open(material_name + "_sword.json", "x")
     pickaxe_file = open(material_name + "_pickaxe.json", "x")
     axe_file = open(material_name + "_axe.json", "x")
@@ -239,24 +238,211 @@ def pop_tool_set(material_name, mod_id):
     
     sword_file.write(
     """
-
-    """    
-    )
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "X",
+    "X",
+    "#"
+  ],
+  "key": {
+    "#": {
+      "item": "minecraft:stick"
+    },
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_sword"
+  }
+}
+    """)
     pickaxe_file.write(
     """
-
-    """    
-    )
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "XXX",
+    " # ",
+    " # "
+  ],
+  "key": {
+    "#": {
+      "item": "minecraft:stick"
+    },
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_pickaxe"
+  }
+}
+    """)
+    axe_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "XX",
+    "X#",
+    " #"
+  ],
+  "key": {
+    "#": {
+      "item": "minecraft:stick"
+    },
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_axe"
+  }
+}
+    """)
+    shovel_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "X",
+    "#",
+    "#"
+  ],
+  "key": {
+    "#": {
+      "item": "minecraft:stick"
+    },
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_shovel"
+  }
+}
+    """)
+    hoe_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "XX",
+    " #",
+    " #"
+  ],
+  "key": {
+    "#": {
+      "item": "minecraft:stick"
+    },
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_hoe"
+  }
+}
+    """)
     
     sword_file.close()
     pickaxe_file.close()
     axe_file.close()
     shovel_file.close()
-    hoe_file.close()
-    
+    hoe_file.close() 
 
+
+def pop_armor_set(material_name, material_id, mod_id):
+    helmet_file = open(material_name + "_helmet.json", "x")
+    chestplate_file = open(material_name + "_chestplate.json", "x")
+    leggings_file = open(material_name + "_leggings.json", "x")
+    boots_file = open(material_name + "_boots.json", "x")
+    
+    helmet_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "XXX",
+    "X X"
+  ],
+  "key": {
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_helmet"
+  }
+}
+    """)
+    chestplate_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "X X",
+    "XXX",
+    "XXX"
+  ],
+  "key": {
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_chestplate"
+  }
+}
+    """)
+    leggings_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "XXX",
+    "X X",
+    "X X"
+  ],
+  "key": {
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_leggings"
+  }
+}
+    """)
+    boots_file.write(
+    """
+{
+  "type": "minecraft:crafting_shaped",
+  "pattern": [
+    "X X",
+    "X X"
+  ],
+  "key": {
+    "X": {
+      "item": \""""+mod_id+""":"""+material_id+""""
+    }
+  },
+  "result": {
+    "item": \""""+mod_id+""":"""+material_name+"""_boots"
+  }
+}
+    """)
+
+    helmet_file.close()
+    chestplate_file.close()
+    leggings_file.close()
+    boots_file.close()
+    
+    
 while True:
-    selection = input("\n\n What do you want to add?\n  0.) Quit\n  1.) Populate a block set\n ")
+    selection = input("\n\n What do you want to add?\n  0.) Quit\n  1.) Populate a block set\n  2.) Populate a tool set\n  3.) Populate an armor set\n ")
     if selection == "0":
         break
     elif selection == "1":
@@ -265,6 +451,16 @@ while True:
         wall_bool = (input("  Are there walls?\n ").lower() == "y")
         cracked_bool = (input("  Are there cracked variants?\n ").lower() == "y")
         pop_block_set(id, fence_bool, wall_bool, cracked_bool)
+        os.system('cls')
+    elif selection == "2":
+        name = input("  What is the tool set's name?\n ").lower()
+        id = input("  What is the tool material's id?\n ").lower()
+        pop_tool_set(name, id, mod_id)
+        os.system('cls')
+    elif selection == "3":
+        name = input("  What is the armor set's name?\n ").lower()
+        id = input("  What is the armor material's id?\n ").lower()
+        pop_armor_set(name, id, mod_id)
         os.system('cls')
     else:
         print(" Enter valid selection!")
