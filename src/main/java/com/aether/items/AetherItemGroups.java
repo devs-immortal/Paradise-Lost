4a14ee5768b2b6679dfac8c959a9297b9f5569df
Nailuj29 Gaming <nailuj29@nailuj29gaming@gmail.com>
2021-04-09 15:15:43 -0400

    Merge some item groups

--- src/main/java/com/aether/items/AetherItemGroups.java
+++ src/main/java/com/aether/items/AetherItemGroups.java
@@ -15,21 +15,21 @@ public class AetherItemGroups {
             Aether.locate("aether_tools"),
             () -> new ItemStack(AetherItems.GRAVITITE_PICKAXE));
 
-    public static final ItemGroup Weapons = FabricItemGroupBuilder.build(
-            Aether.locate("aether_weapons"),
-            () -> new ItemStack(AetherItems.GRAVITITE_SWORD));
+//    public static final ItemGroup Weapons = FabricItemGroupBuilder.build(
+//            Aether.locate("aether_weapons"),
+//            () -> new ItemStack(AetherItems.GRAVITITE_SWORD));
 
-    public static final ItemGroup Armor = FabricItemGroupBuilder.build(
-            Aether.locate("aether_armor"),
-            () -> new ItemStack(AetherItems.ZANITE_CHESTPLATE));
+//    public static final ItemGroup Armor = FabricItemGroupBuilder.build(
+//            Aether.locate("aether_armor"),
+//            () -> new ItemStack(AetherItems.ZANITE_CHESTPLATE));
 
     public static final ItemGroup Food = FabricItemGroupBuilder.build(
             Aether.locate("aether_food"),
             () -> new ItemStack(AetherItems.BLUEBERRY));
 
-    public static final ItemGroup Accessories = FabricItemGroupBuilder.build(
-            Aether.locate("aether_accessories"),
-            () -> new ItemStack(AetherItems.ZANITE_RING));
+//    public static final ItemGroup Accessories = FabricItemGroupBuilder.build(
+//            Aether.locate("aether_accessories"),
+//            () -> new ItemStack(AetherItems.ZANITE_RING));
 
     public static final ItemGroup Resources = FabricItemGroupBuilder.build(
             Aether.locate("aether_resources"),
@@ -38,4 +38,8 @@ public class AetherItemGroups {
     public static final ItemGroup Misc = FabricItemGroupBuilder.build(
             Aether.locate("aether_misc"),
             () -> new ItemStack(AetherItems.BRONZE_KEY));
+
+    public static final ItemGroup Wearable = FabricItemGroupBuilder.build(
+            Aether.locate("aether_wearables"),
+            () -> new ItemStack(AetherItems.ZANITE_CHESTPLATE));
 }
