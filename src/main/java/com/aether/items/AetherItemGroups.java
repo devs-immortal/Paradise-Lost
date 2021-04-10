401c429ceddbf5e9404e4c8c7cec88441cad3f0a
Nailuj29 Gaming <nailuj29@nailuj29gaming@gmail.com>
2021-04-10 13:27:23 -0400

    Squash commits

--- src/main/java/com/aether/items/AetherItemGroups.java
+++ src/main/java/com/aether/items/AetherItemGroups.java
@@ -15,21 +15,9 @@ public class AetherItemGroups {
             Aether.locate("aether_tools"),
             () -> new ItemStack(AetherItems.GRAVITITE_PICKAXE));
 
-//    public static final ItemGroup Weapons = FabricItemGroupBuilder.build(
-//            Aether.locate("aether_weapons"),
-//            () -> new ItemStack(AetherItems.GRAVITITE_SWORD));
-
-//    public static final ItemGroup Armor = FabricItemGroupBuilder.build(
-//            Aether.locate("aether_armor"),
-//            () -> new ItemStack(AetherItems.ZANITE_CHESTPLATE));
-
     public static final ItemGroup Food = FabricItemGroupBuilder.build(
             Aether.locate("aether_food"),
             () -> new ItemStack(AetherItems.BLUEBERRY));
-
-//    public static final ItemGroup Accessories = FabricItemGroupBuilder.build(
-//            Aether.locate("aether_accessories"),
-//            () -> new ItemStack(AetherItems.ZANITE_RING));
 
     public static final ItemGroup Resources = FabricItemGroupBuilder.build(
             Aether.locate("aether_resources"),
