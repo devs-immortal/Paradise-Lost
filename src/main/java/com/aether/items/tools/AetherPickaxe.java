e91f17eed2fa06cc7553dbcee44edf300a171cd4
Jack Papel <64770632+Jack-Papel@users.noreply.github.com>
2021-04-10 19:11:14 -0700

    Update AetherPickaxe.java

--- src/main/java/com/aether/items/tools/AetherPickaxe.java
+++ src/main/java/com/aether/items/tools/AetherPickaxe.java
@@ -3,6 +3,7 @@ package com.aether.items.tools;
 import com.aether.entities.block.FloatingBlockEntity;
 import com.aether.items.utils.AetherTiers;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.PickaxeItem;
@@ -35,6 +36,12 @@ public class AetherPickaxe extends PickaxeItem implements IAetherTool {
         World world = context.getWorld();
         BlockPos blockPos = context.getBlockPos();
         if (this.getItemMaterial() == AetherTiers.Gravitite && FloatingBlockEntity.gravititeToolUsedOnBlock(context, this)) {
+            PlayerEntity playerEntity = context.getPlayer();
+            if (playerEntity != null) {
+                context.getStack().damage(1, playerEntity, (p) -> {
+                    p.sendToolBreakStatus(context.getHand());
+                });
+            }
             return ActionResult.SUCCESS;
         } else {
             return ActionResult.PASS;
