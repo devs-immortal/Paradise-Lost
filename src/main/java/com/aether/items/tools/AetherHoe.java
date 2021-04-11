65472ee0c291c91bca8327a6c25abd06fa8abdfa
Jack Papel <64770632+Jack-Papel@users.noreply.github.com>
2021-04-10 19:07:17 -0700

    Update AetherHoe.java

--- src/main/java/com/aether/items/tools/AetherHoe.java
+++ src/main/java/com/aether/items/tools/AetherHoe.java
@@ -66,22 +66,7 @@ public class AetherHoe extends HoeItem implements IAetherTool {
         if (this.getItemMaterial() == AetherTiers.Gravitite && FloatingBlockEntity.gravititeToolUsedOnBlock(context, this)) {
             return ActionResult.SUCCESS;
         }
-        if (context.getSide() != Direction.DOWN && world.isAir(blockPos.up())) {
-            BlockState blockState = convertibleBlocks.get(world.getBlockState(blockPos).getBlock());
-
-            if (blockState != null) {
-                PlayerEntity entityPlayer = context.getPlayer();
-
-                world.playSound(entityPlayer, blockPos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-
-                if (!world.isClient) {
-                    world.setBlockState(blockPos, blockState, 11);
-                    if (entityPlayer != null) context.getStack().damage(1, entityPlayer, null);
-                }
-                return ActionResult.SUCCESS;
-            }
-        }
-        return ActionResult.PASS;
+        return super.useOnBlock(context);
     }
 
     @Override
