4b548491e31acb8df459dbc1dbd5725e06f99889
24Chrome <m.batt1@outlook.com>
2021-04-22 18:59:10 -0500

    color fix aerbunny loot tweak
    * Changed color provider for grass items to be hex instead of base 10
    * reduced amount of aercloud aerbunnies drop

--- src/main/java/com/aether/client/rendering/block/AetherColorProvs.java
+++ src/main/java/com/aether/client/rendering/block/AetherColorProvs.java
@@ -15,6 +15,6 @@ public class AetherColorProvs {
         ColorProviderRegistry.BLOCK.register(((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefaultColor()), AetherBlocks.SKYROOT_LEAVES, AetherBlocks.SKYROOT_LEAF_PILE);
         ColorProviderRegistry.BLOCK.register(((state, world, pos, tintIndex) ->  world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.getColor(0.5D, 1.0D)), AetherBlocks.AETHER_GRASS_BLOCK, AetherBlocks.AETHER_GRASS, AetherBlocks.AETHER_TALL_GRASS, AetherBlocks.AETHER_FERN, AetherBlocks.AETHER_BUSH);
         ColorProviderRegistry.ITEM.register(((stack, tintIndex) -> 0xbce632), AetherBlocks.SKYROOT_LEAVES.asItem(), AetherBlocks.SKYROOT_LEAF_PILE.asItem());
-        ColorProviderRegistry.ITEM.register(((stack, tintIndex) -> 11665355), AetherBlocks.AETHER_GRASS_BLOCK.asItem(), AetherBlocks.AETHER_GRASS.asItem(), AetherBlocks.AETHER_TALL_GRASS.asItem(), AetherBlocks.AETHER_FERN.asItem(), AetherBlocks.AETHER_BUSH.asItem());
+        ColorProviderRegistry.ITEM.register(((stack, tintIndex) -> 0xB1FFCB), AetherBlocks.AETHER_GRASS_BLOCK.asItem(), AetherBlocks.AETHER_GRASS.asItem(), AetherBlocks.AETHER_TALL_GRASS.asItem(), AetherBlocks.AETHER_FERN.asItem(), AetherBlocks.AETHER_BUSH.asItem());
     }
 }
