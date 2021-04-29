7b05edbe99880345ebf837e9cbe11c0d0e420902
Jack Papel <jackpapel@live.com>
2021-04-28 21:33:40 -0700

    Add Swets
    Added Swets.
    Right now they use the slime models and textures as placeholders until custom models and textures are created.
    They do everything that they did in the original aether legacy.
    This implementation allows for more swet variants if that should be wanted in the future.
    Things that need to be changed: swet loot tables and spawning.

---
+++ src/main/java/com/aether/entities/hostile/SwetEntity.java
@@ -0,0 +1,75 @@
+package com.aether.entities.hostile;
+
+import com.aether.entities.AetherEntityTypes;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+public class SwetEntity extends SlimeEntity {
+    public int stuckCooldown = 0;
+
+    public SwetEntity(World world){
+        super(AetherEntityTypes.BLUE_SWET, world);
+        init();
+    }
+
+    public SwetEntity(EntityType<? extends SwetEntity> entityType, World world){
+        super(entityType, world);
+        init();
+    }
+
+    private void init(){
+        super.setSize(2, false);
+        getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(25);
+        setHealth(getMaxHealth());
+    }
+
+    public static DefaultAttributeContainer.Builder initAttributes() {
+        return AetherEntityTypes.getDefaultAttributes()
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 8.0D)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.28000000417232513D)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 3.0D)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 25.0D);
+    }
+
+    @Override
+    public void tick(){
+        if(stuckCooldown >= 0){
+            System.out.println(stuckCooldown);
+            --stuckCooldown;
+        }
+        super.tick();
+    }
+
+    @Override
+    public void onPlayerCollision(PlayerEntity player){
+        if (player.getVehicle() == null && stuckCooldown <= 0) {
+            player.startRiding(this, true);
+        } else {
+            super.onPlayerCollision(player);
+        }
+    }
+
+    protected void removePassenger(Entity passenger) {
+        if (passenger instanceof PlayerEntity) {
+            stuckCooldown = 30;
+        }
+    }
+
+    // Prevents the size from being changed
+    @Override
+    protected void setSize(int size, boolean heal){
+        if (heal) {
+            this.setHealth(this.getMaxHealth());
+        }
+    }
+
+    @Override
+    public void remove(){
+        this.removed = true;
+    }
+}
