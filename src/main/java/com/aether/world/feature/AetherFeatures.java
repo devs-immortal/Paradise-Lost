c78fb12b9648f1c7e1b6404eb79fb08e551ee2b5
Haven <haven@hephaestus.dev>
2021-05-05 12:51:25 -0700

    Fixes structure spawning in the Aether.
    Spacing and actual generation need to be tweaked. Important things to note for future endeavors:
    - The `DataVersion` field in each .nbt file must match the current version, at the moment that's 2586.
    - Each new structure must be added to the `structures/structures` field of the `data/the_aether/worldgen/noise_settings/aether_noise.json` file *as well as* the biomes `start` field.

--- src/main/java/com/aether/world/feature/AetherFeatures.java
+++ src/main/java/com/aether/world/feature/AetherFeatures.java
@@ -1,19 +1,14 @@
 package com.aether.world.feature;
 
 import com.aether.Aether;
-import com.aether.world.feature.config.AercloudConfig;
 import com.aether.world.feature.generator.SkyrootTowerGenerator;
 import com.aether.world.feature.generator.WellGenerator;
 import com.aether.world.feature.structure.SkyrootTowerFeature;
 import com.aether.world.feature.structure.WellFeature;
 import com.aether.world.gen.decorator.CrystalTreeIslandDecorator;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
 import net.minecraft.structure.StructurePieceType;
-import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.DecoratorConfig;
@@ -21,12 +16,8 @@ import net.minecraft.world.gen.decorator.NopeDecoratorConfig;
 import net.minecraft.world.gen.feature.*;
 
 public class AetherFeatures {
-    public static Feature<AercloudConfig> AERCLOUD;
-    public static Feature<SingleStateFeatureConfig> LAKE;
-    public static Feature<DefaultFeatureConfig> QUICKSOIL;
-
-    public static StructurePieceType WELL_PIECE;
-    public static StructurePieceType SKYROOT_TOWER_PIECE;
+    public static final StructurePieceType WELL_PIECE = register(WellGenerator.WellPiece::new, "well");
+    public static final StructurePieceType SKYROOT_TOWER_PIECE = register(SkyrootTowerGenerator.TowerPiece::new, "skyroot_tower");
 
     public static void registerFeatures() {
         register("lake", new AetherLakeFeature(SingleStateFeatureConfig.CODEC));
@@ -35,30 +26,23 @@ public class AetherFeatures {
         register("crystal_tree_island", new CrystalTreeIslandFeature(DefaultFeatureConfig.CODEC));
 
         // Decorators
-        registerDecorator("crystal_tree_island", new CrystalTreeIslandDecorator(NopeDecoratorConfig.CODEC));
+        register("crystal_tree_island", new CrystalTreeIslandDecorator(NopeDecoratorConfig.CODEC));
 
-        WELL_PIECE = registerStructure("well", WellGenerator.WellPiece::new, new WellFeature(DefaultFeatureConfig.CODEC), 213769);
-        SKYROOT_TOWER_PIECE = registerStructure("skyroot_tower", SkyrootTowerGenerator.TowerPiece::new, new SkyrootTowerFeature(DefaultFeatureConfig.CODEC), 4208012);
+        register("well", new WellFeature(DefaultFeatureConfig.CODEC));
+        register("skyroot_tower", new SkyrootTowerFeature(DefaultFeatureConfig.CODEC));
     }
 
-    private static StructurePieceType registerStructure(String id, StructurePieceType piece, StructureFeature<DefaultFeatureConfig> structure, int salt) {
-        final ConfiguredStructureFeature<?, ?> CONFIGURED = structure.configure(DefaultFeatureConfig.DEFAULT);
-
-        Registry.register(Registry.STRUCTURE_PIECE, Aether.locate(id + "_piece"), piece);
+    private static <T extends FeatureConfig> void register(String id, StructureFeature<T> structure) {
         FabricStructureBuilder.create(Aether.locate(id), structure)
                 .step(GenerationStep.Feature.SURFACE_STRUCTURES)
-                .defaultConfig(32, 8, salt)
-                .adjustsSurface()
+                .defaultConfig(10, 1, 0)
                 .register();
-
-        RegistryKey<ConfiguredStructureFeature<?, ?>> configured = RegistryKey.of(Registry.CONFIGURED_STRUCTURE_FEATURE_WORLDGEN,
-                Aether.locate(id));
-
-        BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE, configured.getValue(), CONFIGURED);
-        BiomeModifications.addStructure(BiomeSelectors.all(), configured);
-
-        return WELL_PIECE;
     }
+
+    static StructurePieceType register(StructurePieceType pieceType, String id) {
+        return Registry.register(Registry.STRUCTURE_PIECE, Aether.locate(id), pieceType);
+    }
+
 
     @SuppressWarnings("UnusedReturnValue")
     private static <C extends FeatureConfig, F extends Feature<C>> F register(String id, F feature) {
@@ -66,7 +50,7 @@ public class AetherFeatures {
         return Registry.register(Registry.FEATURE, Aether.locate(id), feature);
     }
 
-    private static <T extends DecoratorConfig, G extends Decorator<T>> G registerDecorator(String id, G decorator) {
+    private static <T extends DecoratorConfig, G extends Decorator<T>> G register(String id, G decorator) {
         return Registry.register(Registry.DECORATOR, Aether.locate(id), decorator);
     }
 
