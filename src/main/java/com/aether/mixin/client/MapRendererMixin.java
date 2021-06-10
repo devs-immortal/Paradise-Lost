e8adc2a1e528042865aabfb9bc4850dc8dc49785
CDAGaming <cstack2011@yahoo.com>
2021-06-10 13:21:50 -0500

    [Fix] Mapped MapRendererMixin

--- src/main/java/com/aether/mixin/client/MapRendererMixin.java
+++ src/main/java/com/aether/mixin/client/MapRendererMixin.java
@@ -12,24 +12,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net.minecraft.client.gui.MapRenderer$MapTexture")
+@Mixin(targets = "net.minecraft.client.render.MapRenderer$MapTexture")
 public class MapRendererMixin {
 
     @Shadow
     @Final
-    private MapState mapState;
+    private MapState state;
 
     @Shadow
     @Final
     private NativeImageBackedTexture texture;
 
-    @Inject(method = "updateTexture()V", at = @At(value = "RETURN", target = "net/minecraft/client/gui/MapRenderer$MapTexture.updateTexture()V"))
+    @Inject(method = "updateTexture()V", at = @At(value = "RETURN", target = "net/minecraft/client/render/MapRenderer$MapTexture.updateTexture()V"))
     private void updateAetherTexture(CallbackInfo ci) {
-        boolean isAether = this.mapState.dimension == AetherDimension.AETHER_WORLD_KEY;
+        boolean isAether = this.state.dimension == AetherDimension.AETHER_WORLD_KEY;
         for (int int_1 = 0; int_1 < 128; ++int_1) {
             for (int int_2 = 0; int_2 < 128; ++int_2) {
                 int int_3 = int_2 + int_1 * 128;
-                int int_4 = this.mapState.colors[int_3] & 255;
+                int int_4 = this.state.colors[int_3] & 255;
 
                 if (int_4 / 4 == 0) {
                     this.texture.getImage().setPixelColor(int_2, int_1, (int_3 + int_3 / 128 & 1) * 8 + 16 << 24);
