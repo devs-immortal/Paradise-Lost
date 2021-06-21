2f83955ddb619292d1e7f7bc4b3c222e0090609f
CDAGaming <cstack2011@yahoo.com>
2021-06-21 08:14:48 -0500

    [Fix] Fixed model registration

--- src/main/java/com/aether/client/rendering/entity/layer/AetherModelLayers.java
+++ src/main/java/com/aether/client/rendering/entity/layer/AetherModelLayers.java
@@ -1,27 +1,35 @@
 package com.aether.client.rendering.entity.layer;
 
 import com.aether.Aether;
+import com.aether.client.model.entity.AerbunnyModel;
+import com.google.common.collect.Maps;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.util.Identifier;
 
+import java.util.Map;
+
 @Environment(EnvType.CLIENT)
 public class AetherModelLayers {
-    public static final EntityModelLayer AERBUNNY = register("aerbunny", "main");
+    public static final Map<EntityModelLayer, TexturedModelData> ENTRIES = Maps.newHashMap();
 
-    public static EntityModelLayer register(Identifier id, String layer) {
+    public static final EntityModelLayer AERBUNNY = register("aerbunny", "main", AerbunnyModel.getTexturedModelData());
+
+    public static EntityModelLayer register(Identifier id, String layer, TexturedModelData data) {
         EntityModelLayer entityModelLayer = new EntityModelLayer(id, layer);
         if (!EntityModelLayers.LAYERS.add(entityModelLayer)) {
             throw new IllegalStateException("Duplicate registration for " + entityModelLayer);
         } else {
+            ENTRIES.put(entityModelLayer, data);
             return entityModelLayer;
         }
     }
 
-    public static EntityModelLayer register(String id, String layer) {
-        return register(Aether.locate(id), layer);
+    public static EntityModelLayer register(String id, String layer, TexturedModelData data) {
+        return register(Aether.locate(id), layer, data);
     }
 
     public static void initClient() {
