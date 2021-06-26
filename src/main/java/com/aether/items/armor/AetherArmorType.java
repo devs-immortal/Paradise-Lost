4b42ea36052a5a4fa9e413e2000e0fa085f677a9
CDAGaming <cstack2011@yahoo.com>
2021-06-26 11:51:21 -0500

    [Change] Buffed Obsidian and Valkyrie Tiers
    - Both now use the Netherite equip sound effect, and adopt a 0.1F knockback resistance as their stats are closer to that then diamond.

--- src/main/java/com/aether/items/armor/AetherArmorType.java
+++ src/main/java/com/aether/items/armor/AetherArmorType.java
@@ -6,13 +6,12 @@ import net.minecraft.item.ArmorMaterial;
 import net.minecraft.sound.SoundEvents;
 
 public enum AetherArmorType {
-    //TODO: Maybe tweak knockback resistance
     Zanite(new AetherArmorMaterial("zanite", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, AetherItems.ZANITE_GEM, 0), 0x711AE8),
     Gravitite(new AetherArmorMaterial("gravitite", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, AetherItems.GRAVITITE_GEM, 0), 0xe752DB),
     Neptune(new AetherArmorMaterial("neptune", 35, new int[]{4, 6, 8, 4}, 8, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.5F, Blocks.AIR, 0), 0x2654FF),
     Phoenix(new AetherArmorMaterial("phoenix", 35, new int[]{4, 6, 8, 4}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.5F, Blocks.AIR, 0), 0xFF7700),
-    Obsidian(new AetherArmorMaterial("obsidian", 38, new int[]{4, 8, 10, 4}, 6, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, Blocks.AIR, 0), 0x1B1447),
-    Valkyrie(new AetherArmorMaterial("valkyrie", 35, new int[]{4, 8, 10, 4}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.5F, Blocks.AIR, 0), 0xDDDDDD),
+    Obsidian(new AetherArmorMaterial("obsidian", 38, new int[]{4, 8, 10, 4}, 6, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 2.0F, Blocks.AIR, 0.1F), 0x1B1447),
+    Valkyrie(new AetherArmorMaterial("valkyrie", 35, new int[]{4, 8, 10, 4}, 10, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 1.5F, Blocks.AIR, 0.1F), 0xDDDDDD),
     Sentry(new AetherArmorMaterial("sentry", 35, new int[]{4, 8, 10, 4}, 10, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 1.5F, Blocks.AIR, 0), 0x707070);
 
     private final ArmorMaterial material;
