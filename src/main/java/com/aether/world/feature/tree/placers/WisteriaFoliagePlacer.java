4cd03b8cfdba4dc83e4ea76461c0fb3ab11e0601
CDAGaming <cstack2011@yahoo.com>
2021-06-26 12:48:32 -0500

    [Fix] Hopefully fixed Wisteria Foilage

--- src/main/java/com/aether/world/feature/tree/placers/WisteriaFoliagePlacer.java
+++ src/main/java/com/aether/world/feature/tree/placers/WisteriaFoliagePlacer.java
@@ -1,18 +1,27 @@
 package com.aether.world.feature.tree.placers;
 
+import com.aether.blocks.natural.AetherLeavesBlock;
+import com.aether.blocks.natural.AuralLeavesBlock;
 import com.aether.world.feature.tree.AetherTreeHell;
+import com.google.common.collect.Sets;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.intprovider.IntProvider;
 import net.minecraft.world.TestableWorld;
+import net.minecraft.world.gen.feature.TreeFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
 import net.minecraft.world.gen.foliage.FoliagePlacerType;
 
 import java.util.Random;
+import java.util.Set;
 import java.util.function.BiConsumer;
+
+import static com.aether.blocks.natural.AetherHangerBlock.TIP;
 
 public class WisteriaFoliagePlacer extends FoliagePlacer {
 
@@ -30,52 +39,48 @@ public class WisteriaFoliagePlacer extends FoliagePlacer {
 
     @Override
     protected void generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, TreeFeatureConfig config, int trunkHeight, TreeNode treeNode, int foliageHeight, int radius, int offset) {
-        // TODO: FIXME - 1.17 (Using Stub code for now)
-        boolean bl = treeNode.isGiantTrunk();
-        BlockPos blockPos = treeNode.getCenter().up(offset);
-        this.generateSquare(world, replacer, random, config, blockPos, radius + treeNode.getFoliageRadius(), -1 - foliageHeight, bl);
-        this.generateSquare(world, replacer, random, config, blockPos, radius - 1, -foliageHeight, bl);
-        this.generateSquare(world, replacer, random, config, blockPos, radius + treeNode.getFoliageRadius() - 1, 0, bl);
-//        if(radius <= 3)
-//            radius = 3;
-//
-//        radius -= treeNode.getFoliageRadius();
-//        BlockPos nodePos = treeNode.getCenter();
-//        BlockPos altNodePos = nodePos.add(0, 1, 0);
-//        BlockState leafBlock = config.leavesProvider.getBlockState(random, nodePos);
-//        BlockState hanger = Blocks.AIR.getDefaultState();
-//
-//        if(leafBlock.getBlock() instanceof AetherLeavesBlock || leafBlock.getBlock() instanceof AuralLeavesBlock) {
-//            hanger = AetherLeavesBlock.getHanger(leafBlock);
-//        }
-//
-//        for(int i = -radius; i <= radius; i++) {
-//            for (int j = -radius; j <= radius; j++) {
-//                for (int k = 0; k < radius; k++) {
-//                    BlockPos offPos = nodePos.add(Math.signum(i) * Math.abs(i)-k, k, Math.signum(j) * Math.abs(j)-k);
-//                    if((world.testBlockState(offPos, AbstractBlock.AbstractBlockState::isAir) || TreeFeature.canReplace(world, offPos)) && offPos.isWithinDistance(random.nextBoolean() ? nodePos : altNodePos, radius)) {
-//                        world.setBlockState(offPos, leafBlock, 19);
-//                        leaves.add(offPos);
-//                    }
-//                }
-//            }
-//        }
-//        for (int i = -radius; i < radius; i++) {
-//            for (int j = -radius; j < radius; j++) {
-//                BlockPos offPos = nodePos.add(i, 0, j);
-//                if(leaves.contains(offPos) && random.nextBoolean()) {
-//                    offPos = offPos.down();
-//                    int hangerLength = 1 + random.nextInt(2);
-//                    int step = 0;
-//                    while (step <= hangerLength && world.testBlockState(offPos, AbstractBlock.AbstractBlockState::isAir)) {
-//                        world.setBlockState(offPos, hanger.with(TIP, false), 19);
-//                        offPos = offPos.down();
-//                        step++;
-//                    }
-//                    world.setBlockState(offPos.up(), hanger.with(TIP, true), 19);
-//                }
-//            }
-//        }
+        Set<BlockPos> leaves = Sets.newHashSet();
+        if(radius <= 3)
+            radius = 3;
+
+        radius -= treeNode.getFoliageRadius();
+        BlockPos nodePos = treeNode.getCenter();
+        BlockPos altNodePos = nodePos.up(offset);
+
+        BlockState leafBlock = config.foliageProvider.getBlockState(random, nodePos);
+        BlockState hanger = Blocks.AIR.getDefaultState();
+
+        if(leafBlock.getBlock() instanceof AetherLeavesBlock || leafBlock.getBlock() instanceof AuralLeavesBlock) {
+            hanger = AetherLeavesBlock.getHanger(leafBlock);
+        }
+
+        for(int i = -radius; i <= radius; i++) {
+            for (int j = -radius; j <= radius; j++) {
+                for (int k = 0; k < radius; k++) {
+                    BlockPos offPos = nodePos.add(Math.signum(i) * Math.abs(i)-k, k, Math.signum(j) * Math.abs(j)-k);
+                    if((world.testBlockState(offPos, AbstractBlock.AbstractBlockState::isAir) || TreeFeature.canReplace(world, offPos)) && offPos.isWithinDistance(random.nextBoolean() ? nodePos : altNodePos, radius)) {
+                        replacer.accept(offPos, leafBlock);
+                        leaves.add(offPos);
+                    }
+                }
+            }
+        }
+        for (int i = -radius; i < radius; i++) {
+            for (int j = -radius; j < radius; j++) {
+                BlockPos offPos = nodePos.add(i, 0, j);
+                if(leaves.contains(offPos) && random.nextBoolean()) {
+                    offPos = offPos.down();
+                    int hangerLength = 1 + random.nextInt(2);
+                    int step = 0;
+                    while (step <= hangerLength && world.testBlockState(offPos, AbstractBlock.AbstractBlockState::isAir)) {
+                        replacer.accept(offPos, hanger.with(TIP, false));
+                        offPos = offPos.down();
+                        step++;
+                    }
+                    replacer.accept(offPos.up(), hanger.with(TIP, true));
+                }
+            }
+        }
     }
 
     @Override
