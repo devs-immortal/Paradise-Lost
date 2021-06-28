f0e39efdb19b92eb8d783111bbf177fe94716ff1
Jack Papel <jackpapel@live.com>
2021-06-27 17:17:44 -0700

    ServerPlayerEntity * gravitite changes
    Now the server player entity is also affected by the gravitite changes, which hopefully should allow things to work.

---
+++ src/main/java/com/aether/mixin/server/ServerPlayerEntityMixin.java
@@ -0,0 +1,26 @@
+package com.aether.mixin.server;
+
+import com.aether.entities.AetherEntityExtensions;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin extends Entity {
+    public ServerPlayerEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void tick(CallbackInfo ci){
+        if (((AetherEntityExtensions)this).flipped){
+            this.setVelocity(this.getVelocity().add(0,0.12D,0));
+        }
+    }
+}
