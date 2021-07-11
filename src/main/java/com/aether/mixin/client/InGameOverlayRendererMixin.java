0db5702a099576381efd49e77fdd25965551f5f7
Jack Papel <jackpapel@live.com>
2021-07-11 14:17:36 -0700

    Fluid rendering changes
    Still water will now have the same fading effects as flowing water in the Aether.
    When swimming in dense aercloud, there's now an fitting overlay such that it doesn't look so odd. At the moment, the overlay is the dense aercloud texture, but probably should be changed to something custom.
    I couldn't figure out how to remove the effect that makes the screen blue, so that remains.

--- src/main/java/com/aether/mixin/client/InGameOverlayRendererMixin.java
+++ src/main/java/com/aether/mixin/client/InGameOverlayRendererMixin.java
@@ -1,6 +1,7 @@
 package com.aether.mixin.client;
 
 import com.aether.blocks.aercloud.BaseAercloudBlock;
+import com.aether.blocks.aercloud.DenseAercloudFluid;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -15,6 +16,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -71,6 +73,34 @@ public abstract class InGameOverlayRendererMixin {
                 cir.setReturnValue(blockState);
                 cir.cancel();
             }
+        }
+    }
+
+    @Inject(method = "renderUnderwaterOverlay", at = @At("HEAD"), cancellable = true)
+    private static void renderDenseAercloudOverlay(MinecraftClient client, MatrixStack matrices, CallbackInfo ci){
+        BlockPos pos = new BlockPos(client.player.getEyePos());
+        World world = client.player.world;
+        if(world.getFluidState(pos).getFluid() instanceof DenseAercloudFluid){
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.enableTexture();
+            RenderSystem.setShaderTexture(0, new Identifier("the_aether","textures/block/dense_aercloud_still.png"));
+            BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+            float f = client.player.getBrightnessAtEyes();
+            RenderSystem.enableBlend();
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.setShaderColor(f, f, f, 0.8F);
+            float m = -client.player.getYaw() / 64.0F;
+            float n = client.player.getPitch() / 64.0F;
+            Matrix4f matrix4f = matrices.peek().getModel();
+            bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+            bufferBuilder.vertex(matrix4f, -1.0F, -1.0F, -0.5F).texture(4.0F + m, 4.0F + n).next();
+            bufferBuilder.vertex(matrix4f, 1.0F, -1.0F, -0.5F).texture(0.0F + m, 4.0F + n).next();
+            bufferBuilder.vertex(matrix4f, 1.0F, 1.0F, -0.5F).texture(0.0F + m, 0.0F + n).next();
+            bufferBuilder.vertex(matrix4f, -1.0F, 1.0F, -0.5F).texture(4.0F + m, 0.0F + n).next();
+            bufferBuilder.end();
+            BufferRenderer.draw(bufferBuilder);
+            RenderSystem.disableBlend();
+            ci.cancel();
         }
     }
 }
