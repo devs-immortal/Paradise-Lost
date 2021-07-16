abdf6e4cfaaaf9e7bd2ba21e956792661d382110
Solly Watkins <solly@sollyw.com>
2021-07-16 21:00:16 +1000

    Add pressure plates and buttons for aether wood types

---
+++ src/main/java/com/aether/blocks/decorative/AetherWoodenButtonBlock.java
@@ -0,0 +1,9 @@
+package com.aether.blocks.decorative;
+
+import net.minecraft.block.WoodenButtonBlock;
+
+public class AetherWoodenButtonBlock extends WoodenButtonBlock {
+    public AetherWoodenButtonBlock(Settings settings) {
+        super(settings);
+    }
+}
