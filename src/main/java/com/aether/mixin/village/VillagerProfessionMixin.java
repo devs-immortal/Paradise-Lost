e8e3947ef824efd616a343ce3d99415c24ecd5ac
Solly Watkins <solly@sollyw.com>
2021-07-20 21:25:51 +1000

    villagers farm on aether farmland

--- src/main/java/com/aether/mixin/village/VillagerProfessionMixin.java
+++ src/main/java/com/aether/mixin/village/VillagerProfessionMixin.java
@@ -1,5 +1,6 @@
 package com.aether.mixin.village;
 
+import com.aether.village.AetherVillagerProfessionExtensions;
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.Block;
 import net.minecraft.village.VillagerProfession;
@@ -9,12 +10,13 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(VillagerProfession.class)
-public abstract class VillagerProfessionMixin {
+public abstract class VillagerProfessionMixin implements AetherVillagerProfessionExtensions {
     @Shadow
     @Mutable
     @Final
     private ImmutableSet<Block> secondaryJobSites;
 
+    @Override
     public void addSecondaryJobSite(Block jobSite) {
         secondaryJobSites = ImmutableSet.<Block>builder().addAll(secondaryJobSites).add(jobSite).build();
     }
---
+++ src/main/java/com/aether/village/AetherVillagerProfessionExtensions.java
@@ -0,0 +1,7 @@
+package com.aether.village;
+
+import net.minecraft.block.Block;
+
+public interface AetherVillagerProfessionExtensions {
+    void addSecondaryJobSite(Block jobSite);
+}
