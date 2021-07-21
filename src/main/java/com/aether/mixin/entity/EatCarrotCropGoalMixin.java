8cc84179aad65b50562b6bb0b21449bb93fde490
Solly Watkins <solly@sollyw.com>
2021-07-21 17:18:55 +1000

    clean up rabbit mixin

--- src/main/java/com/aether/mixin/entity/EatCarrotCropGoalMixin.java
+++ src/main/java/com/aether/mixin/entity/EatCarrotCropGoalMixin.java
@@ -3,14 +3,13 @@ package com.aether.mixin.entity;
 import com.aether.blocks.AetherBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.passive.RabbitEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RabbitEntity.EatCarrotCropGoal.class)
+@Mixin(targets = "net/minecraft/entity/passive/RabbitEntity$EatCarrotCropGoal")
 public abstract class EatCarrotCropGoalMixin {
-    @Redirect(method = "isTargetPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
+    @Redirect(method = "isTargetPos(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
     public boolean isThisOrAetherFarmland(BlockState self, Block block) {
         return self.isOf(block) || self.isOf(AetherBlocks.AETHER_FARMLAND);
     }
