4e244605676a9001461d60a682e3953306c5a2a9
Solly Watkins <solly@sollyw.com>
2021-07-27 17:12:41 +1000

    a

--- src/main/java/com/aether/util/AetherSignType.java
+++ src/main/java/com/aether/util/AetherSignType.java
@@ -1,20 +1,16 @@
 package com.aether.util;
 
+import com.aether.mixin.util.SignTypeAccessor;
 import net.minecraft.util.SignType;
 
 public class AetherSignType extends SignType {
-    public static final AetherSignType SKYROOT = registerSign(new AetherSignType("skyroot"));
-    public static final AetherSignType ORANGE = registerSign(new AetherSignType("orange"));
-    public static final AetherSignType WISTERIA = registerSign(new AetherSignType("wisteria"));
-    public static final AetherSignType GOLDEN_OAK = registerSign(new AetherSignType("golden_oak"));
-    public static final AetherSignType CRYSTAL = registerSign(new AetherSignType("crystal"));
+    public static final SignType SKYROOT = SignTypeAccessor.callRegister(new AetherSignType("aether_skyroot"));
+    public static final SignType ORANGE = SignTypeAccessor.callRegister(new AetherSignType("aether_orange"));
+    public static final SignType WISTERIA = SignTypeAccessor.callRegister(new AetherSignType("aether_wisteria"));
+    public static final SignType GOLDEN_OAK = SignTypeAccessor.callRegister(new AetherSignType("aether_golden_oak"));
+    public static final SignType CRYSTAL = SignTypeAccessor.callRegister(new AetherSignType("aether_crystal"));
 
     protected AetherSignType(String name) {
         super(name);
-    }
-
-    public static AetherSignType registerSign(AetherSignType type){
-        VALUES.add(type);
-        return type;
     }
 }
