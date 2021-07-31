c532a815051ff21365397e67feb2ae0d25980acf
Solly Watkins <solly@sollyw.com>
2021-07-31 16:09:12 +1000

    fix mixin mapping issues

--- src/main/java/com/aether/mixin/entity/RandomLookGoalMixin.java
+++ src/main/java/com/aether/mixin/entity/RandomLookGoalMixin.java
@@ -1,20 +1,23 @@
 package com.aether.mixin.entity;
 
 import com.aether.entities.hostile.SwetEntity;
-import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.mob.SlimeEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net/minecraft/entity/mob/SlimeEntity$RandomLookGoal")
 public class RandomLookGoalMixin {
+    @Shadow
+    private int timer;
+
     @SuppressWarnings("UnnecessaryQualifiedMemberReference")
     @Inject(method = "Lnet/minecraft/entity/mob/SlimeEntity$RandomLookGoal;<init>(Lnet/minecraft/entity/mob/SlimeEntity;)V", at = @At("RETURN"))
     private void saveReference(SlimeEntity slime, CallbackInfo ci) {
         if (slime instanceof SwetEntity swet) {
-            swet.randomLookGoal = (Goal) (Object) this;
+            swet.setRandomLookTimer = value -> this.timer = value;
         }
     }
 }
