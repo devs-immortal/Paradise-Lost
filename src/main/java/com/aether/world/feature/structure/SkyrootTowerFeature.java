f9ec10701691904ecda5871152b5f45a0a07a234
24Chrome <m.batt1@outlook.com>
2021-08-04 17:43:42 -0500

    #328 center towers

--- src/main/java/com/aether/world/feature/structure/SkyrootTowerFeature.java
+++ src/main/java/com/aether/world/feature/structure/SkyrootTowerFeature.java
@@ -1,15 +1,23 @@
 package com.aether.world.feature.structure;
 
+import com.aether.Aether;
 import com.aether.world.feature.generator.SkyrootTowerGenerator;
 import com.mojang.serialization.Codec;
+import net.minecraft.structure.Structure;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.structure.StructureStart;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
@@ -26,6 +34,15 @@ public class SkyrootTowerFeature extends StructureFeature<DefaultFeatureConfig> 
     }
 
     @Override
+    protected boolean shouldStartAt(ChunkGenerator chunkGenerator, BiomeSource biomeSource, long worldSeed, ChunkRandom random, ChunkPos pos, Biome biome, ChunkPos chunkPos, DefaultFeatureConfig config, HeightLimitView world) {
+        if (chunkGenerator.getHeight(chunkPos.x*16, chunkPos.z*16, Heightmap.Type.WORLD_SURFACE_WG, world) < 7) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
     public GenerationStep.Feature getGenerationStep() {
         return GenerationStep.Feature.SURFACE_STRUCTURES;
     }
@@ -38,11 +55,14 @@ public class SkyrootTowerFeature extends StructureFeature<DefaultFeatureConfig> 
 
         @Override
         public void init(DynamicRegistryManager registryManager, ChunkGenerator chunkGenerator, StructureManager manager, ChunkPos pos, Biome biome, DefaultFeatureConfig config, HeightLimitView world) {
-            int x = pos.x * 16;
-            int z = pos.z * 16;
-            int y = chunkGenerator.getHeight(x, z, Heightmap.Type.WORLD_SURFACE_WG, world);
-            BlockPos newPos = new BlockPos(x, y, z);
-            SkyrootTowerGenerator.addPieces(manager, this, random, newPos);
+            Structure structure = manager.getStructureOrBlank(Aether.locate("skyroot_tower"));
+            BlockRotation blockRotation = BlockRotation.NONE;
+            BlockPos pivot = new BlockPos(structure.getSize().getX() / 2, 0, structure.getSize().getZ() / 2);
+            BlockBox boundingBox = structure.calculateBoundingBox(pos.getStartPos(), blockRotation, pivot, BlockMirror.NONE);
+            BlockPos center = boundingBox.getCenter();
+            int y = chunkGenerator.getHeight(pos.getStartPos().getX()-4, pos.getStartPos().getZ()-4, Heightmap.Type.WORLD_SURFACE_WG, world);
+            BlockPos newPos = new BlockPos(pos.getStartPos().getX()-4, y, pos.getStartPos().getZ()-4);
+            SkyrootTowerGenerator.addPieces(manager, this, blockRotation, newPos);
             this.setBoundingBoxFromChildren();
         }
     }
