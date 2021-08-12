ed856d441cc8298abae311a78e77427021584a26
Solly Watkins <solly@sollyw.com>
2021-08-12 12:22:42 +1000

    Revert "Fix the things I broke"

--- src/main/java/com/aether/mixin/item/ItemStackMixin.java
+++ src/main/java/com/aether/mixin/item/ItemStackMixin.java
@@ -9,7 +9,9 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
@@ -19,5 +21,16 @@ public abstract class ItemStackMixin {
     private Formatting getCustomRarityFormattingForHovering(Rarity rarity) {
         if (rarity != null) return rarity.formatting;
         return ((AetherItemExtensions) this.getItem()).getCustomRarityFormatting();
+    }
+
+    @Inject(method = "canCombine", at = @At("TAIL"), cancellable = true)
+    private static void combineStackableVariants(ItemStack stack, ItemStack otherStack, CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValueZ() && stack.isOf(otherStack.getItem())) {
+            ItemStack stackComparable = stack.copy();
+            ItemStack otherStackComparable = otherStack.copy();
+            stackComparable.getOrCreateNbt().remove("stackableVariant");
+            otherStackComparable.getOrCreateNbt().remove("stackableVariant");
+            cir.setReturnValue(ItemStack.areNbtEqual(stackComparable, otherStackComparable));
+        }
     }
 }
