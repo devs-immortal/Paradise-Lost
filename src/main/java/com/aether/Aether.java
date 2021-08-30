d239d4a12122151b45a9a99f4b5e2bac939ff143
Jack Papel <jackpapel@live.com>
2021-08-30 00:27:39 -0700

    Delegate particles to FAPI, fix Golden Oak particles
    One mixin down :thumbsup:

--- src/main/java/com/aether/Aether.java
+++ src/main/java/com/aether/Aether.java
@@ -7,6 +7,7 @@ import com.aether.client.model.AetherArmorModels;
 import com.aether.client.model.AetherModelPredicates;
 import com.aether.client.rendering.entity.AetherEntityRenderers;
 import com.aether.client.rendering.entity.layer.AetherModelLayers;
+import com.aether.client.rendering.particle.AetherParticles;
 import com.aether.commands.AetherCommands;
 import com.aether.entities.AetherEntityTypes;
 import com.aether.fluids.AetherFluids;
@@ -65,5 +66,6 @@ public class Aether implements ModInitializer, ClientModInitializer {
         AetherEntityRenderers.initClient();
         AetherBlockEntityTypes.initClient();
         AetherFluids.initClient();
+        AetherParticles.initClient();
     }
 }
