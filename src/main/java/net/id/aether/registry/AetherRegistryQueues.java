dcc2695913502ee01b9038f1bf7f5e6917474748
Jack Papel <jackpapel@live.com>
2021-09-12 17:31:36 -0700

    Remove unused imports

--- src/main/java/net/id/aether/registry/AetherRegistryQueues.java
+++ src/main/java/net/id/aether/registry/AetherRegistryQueues.java
@@ -1,17 +1,10 @@
 package net.id.aether.registry;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.loader.api.FabricLoader;
 import net.id.incubus_core.util.RegistryQueue;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiConsumer;
 
 public final class AetherRegistryQueues {
     public static final RegistryQueue<Block> BLOCK = new RegistryQueue<>(Registry.BLOCK, 256);
