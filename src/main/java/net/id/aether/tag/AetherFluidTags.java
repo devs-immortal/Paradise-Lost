b80a225b6cff10fde8c948f1bc0cccdcb5dea91d
Jack Papel <jackpapel@live.com>
2021-09-12 19:55:35 -0700

    TagRegistry is deprecated
    Moved to TagFactory

--- src/main/java/net/id/aether/tag/AetherFluidTags.java
+++ src/main/java/net/id/aether/tag/AetherFluidTags.java
@@ -1,14 +1,14 @@
 package net.id.aether.tag;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.id.aether.Aether;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class AetherFluidTags {
-    public static final Tag<Fluid> SWET_TRANSFORMERS_BLUE = TagRegistry.fluid(new Identifier(Aether.MOD_ID, "swet_transformers/blue"));
-    public static final Tag<Fluid> SWET_TRANSFORMERS_GOLDEN = TagRegistry.fluid(new Identifier(Aether.MOD_ID, "swet_transformers/golden"));
-    public static final Tag<Fluid> SWET_TRANSFORMERS_PURPLE = TagRegistry.fluid(new Identifier(Aether.MOD_ID, "swet_transformers/purple"));
-    public static final Tag<Fluid> SWET_TRANSFORMERS_VERMILION = TagRegistry.fluid(new Identifier(Aether.MOD_ID, "swet_transformers/vermilion"));
+    public static final Tag<Fluid> SWET_TRANSFORMERS_BLUE = TagFactory.FLUID.create(new Identifier(Aether.MOD_ID, "swet_transformers/blue"));
+    public static final Tag<Fluid> SWET_TRANSFORMERS_GOLDEN = TagFactory.FLUID.create(new Identifier(Aether.MOD_ID, "swet_transformers/golden"));
+    public static final Tag<Fluid> SWET_TRANSFORMERS_PURPLE = TagFactory.FLUID.create(new Identifier(Aether.MOD_ID, "swet_transformers/purple"));
+    public static final Tag<Fluid> SWET_TRANSFORMERS_VERMILION = TagFactory.FLUID.create(new Identifier(Aether.MOD_ID, "swet_transformers/vermilion"));
 }
--- src/main/java/net/id/aether/tag/AetherItemTags.java
+++ src/main/java/net/id/aether/tag/AetherItemTags.java
@@ -1,16 +1,16 @@
 package net.id.aether.tag;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.id.aether.Aether;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class AetherItemTags {
-    public static final Tag<Item> GROWS_SWETS = TagRegistry.item(new Identifier(Aether.MOD_ID, "grows_swets"));
-    public static final Tag<Item> SWET_TRANSFORMERS_BLUE = TagRegistry.item(new Identifier(Aether.MOD_ID, "swet_transformers/blue"));
-    public static final Tag<Item> SWET_TRANSFORMERS_GOLDEN = TagRegistry.item(new Identifier(Aether.MOD_ID, "swet_transformers/golden"));
-    public static final Tag<Item> SWET_TRANSFORMERS_PURPLE = TagRegistry.item(new Identifier(Aether.MOD_ID, "swet_transformers/purple"));
-    public static final Tag<Item> SWET_TRANSFORMERS_VERMILION = TagRegistry.item(new Identifier(Aether.MOD_ID, "swet_transformers/vermilion"));
-    public static final Tag<Item> PARACHUTES = TagRegistry.item(new Identifier(Aether.MOD_ID, "parachutes"));
+    public static final Tag<Item> GROWS_SWETS = TagFactory.ITEM.create(new Identifier(Aether.MOD_ID, "grows_swets"));
+    public static final Tag<Item> SWET_TRANSFORMERS_BLUE = TagFactory.ITEM.create(new Identifier(Aether.MOD_ID, "swet_transformers/blue"));
+    public static final Tag<Item> SWET_TRANSFORMERS_GOLDEN = TagFactory.ITEM.create(new Identifier(Aether.MOD_ID, "swet_transformers/golden"));
+    public static final Tag<Item> SWET_TRANSFORMERS_PURPLE = TagFactory.ITEM.create(new Identifier(Aether.MOD_ID, "swet_transformers/purple"));
+    public static final Tag<Item> SWET_TRANSFORMERS_VERMILION = TagFactory.ITEM.create(new Identifier(Aether.MOD_ID, "swet_transformers/vermilion"));
+    public static final Tag<Item> PARACHUTES = TagFactory.ITEM.create(new Identifier(Aether.MOD_ID, "parachutes"));
 }
