3a76025f9f4a0ed8aa6ef05e6bfde6536542b830
Jack Papel <jackpapel@live.com>
2021-09-18 23:03:12 -0700

    Dynamic boreal wisteria leaves
    + change from perlin to simplex noise.

--- src/main/java/net/id/aether/blocks/natural/AuralLeavesBlock.java
+++ src/main/java/net/id/aether/blocks/natural/AuralLeavesBlock.java
@@ -4,16 +4,26 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.id.aether.entities.util.RenderUtils;
 import net.id.aether.util.DynamicBlockColorProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3i;
-import net.minecraft.util.math.noise.PerlinNoiseSampler;
-import net.minecraft.world.gen.SimpleRandom;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class AuralLeavesBlock extends AetherLeavesBlock implements DynamicBlockColorProvider {
 
     private final Vec3i[] gradientColors;
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        MinecraftClient.getInstance().worldRenderer.scheduleBlockRenders(pos.getX(), pos.getY(), pos.getZ(), pos.getX(), pos.getY(), pos.getZ());
+        super.randomDisplayTick(state, world, pos, random);
+    }
 
     public AuralLeavesBlock(Settings settings, boolean collidable, Vec3i[] gradientColors) {
         super(settings, collidable);
@@ -29,19 +39,32 @@ public class AuralLeavesBlock extends AetherLeavesBlock implements DynamicBlockC
         return MathHelper.clamp(percent * Math.sqrt((4 + contrast) / (4 + 4 * contrast * percent * percent)) + 0.5, 0, 1);
     }
 
+    protected static double sampleNoise(BlockPos pos, float clumpSize){
+        if (!isFabulousGraphics()) {
+            return 0.5 * (1 + SimplexNoise.noise(pos.getX() / clumpSize, pos.getY() / clumpSize, pos.getZ() / clumpSize));
+        } else {
+            clumpSize += 3;
+            World world = MinecraftClient.getInstance().world;
+            return 0.5 * (1 + SimplexNoise.noise(pos.getX() / clumpSize + world.getTime()/100F, pos.getY() / clumpSize + world.getTime()/100F, pos.getZ() / clumpSize + world.getTime()/100F));
+        }
+    }
+
+    private static boolean isFabulousGraphics(){
+        return MinecraftClient.getInstance().options.graphicsMode.equals(GraphicsMode.FABULOUS);
+    }
+
     public static int getAuralColor(BlockPos pos, Vec3i[] colorRGBs) {
         Vec3i color1 = colorRGBs[0];
         Vec3i color2 = colorRGBs[1];
         Vec3i color3 = colorRGBs[2];
         Vec3i color4 = colorRGBs[3];
-        float clumpSize = 7;
+        float clumpSize = 13;
 
         // First, we mix color 1 and color 2 using noise
-        PerlinNoiseSampler perlinNoise = new PerlinNoiseSampler(new SimpleRandom(1738));
-        // Sample perlin noise (and change bounds from [-1, 1] to [0, 1])
-        double perlin = 0.5 * (1 + perlinNoise.sample(pos.getX() / clumpSize, pos.getY() / clumpSize, pos.getZ() / clumpSize, 4000, 0));
+        // Sample simplex noise (and change bounds from [-1, 1] to [0, 1])
+        double simplex = sampleNoise(pos.up(3300), clumpSize);
         // Reshape contrast curve
-        double percent = contrastCurve(36, perlin);
+        double percent = contrastCurve(36, simplex);
         percent = percent * (2 - percent);
         // Interpolate
         double r1, g1, b1;
@@ -51,10 +74,9 @@ public class AuralLeavesBlock extends AetherLeavesBlock implements DynamicBlockC
 
         // Now we mix colors 3 and 4 together using noise
         // Rinse, repeat as seen above.
-        perlinNoise = new PerlinNoiseSampler(new SimpleRandom(1337));
         // Sample & reshape
-        double perlin2 = MathHelper.clamp(0.5 * (1 + perlinNoise.sample(pos.getX() / clumpSize, pos.getY() / clumpSize, pos.getZ() / clumpSize, 4000, 0)), 0, 1);
-        double percent2 = perlin2 * (2 - perlin2);
+        double simplex2 = MathHelper.clamp(sampleNoise(pos.west(1337), clumpSize), 0, 1);
+        double percent2 = simplex2 * (2 - simplex2);
         // Interpolate
         double r2, g2, b2;
         r2 = (MathHelper.lerp(percent2, color3.getX(), color4.getX()));
@@ -62,9 +84,8 @@ public class AuralLeavesBlock extends AetherLeavesBlock implements DynamicBlockC
         b2 = (MathHelper.lerp(percent2, color3.getZ(), color4.getZ()));
 
         // This last section interpolates between r1, g1, b1, and r2, g2, b2, finally mixing all the colors together.
-        perlinNoise = new PerlinNoiseSampler(new SimpleRandom(9980));
-        double perlin3 = 0.5 * (1 + perlinNoise.sample(pos.getX() / clumpSize, pos.getY() / clumpSize, pos.getZ() / clumpSize, 4000, 0));
-        double finalPercent = contrastCurve(25, perlin3);
+        double simplex3 = sampleNoise(pos.east(1738), clumpSize);
+        double finalPercent = contrastCurve(25, simplex3);
         // Interpolate
         int finalR, finalG, finalB;
         finalR = (int) (MathHelper.lerp(finalPercent, r1, r2));
@@ -78,5 +99,201 @@ public class AuralLeavesBlock extends AetherLeavesBlock implements DynamicBlockC
     @Environment(EnvType.CLIENT)
     public BlockColorProvider getProvider() {
         return (state, world, pos, tintIndex) -> getAuralColor(pos, gradientColors);
+    }
+
+
+    /*
+     * A speed-improved simplex noise algorithm for 2D, 3D and 4D in Java.
+     *
+     * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
+     * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
+     * Better rank ordering method for 4D by Stefan Gustavson in 2012.
+     *
+     * This could be speeded up even further, but it's useful as it is.
+     *
+     * Version 2012-03-09
+     *
+     * This code was placed in the public domain by its original author,
+     * Stefan Gustavson. You may use it as you see fit, but
+     * attribution is appreciated.
+     *
+     */
+
+    private static class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
+        private static Grad grad3[] = {new Grad(1, 1, 0), new Grad(-1, 1, 0), new Grad(1, -1, 0), new Grad(-1, -1, 0),
+                new Grad(1, 0, 1), new Grad(-1, 0, 1), new Grad(1, 0, -1), new Grad(-1, 0, -1),
+                new Grad(0, 1, 1), new Grad(0, -1, 1), new Grad(0, 1, -1), new Grad(0, -1, -1)};
+
+        private static short p[] = {151, 160, 137, 91, 90, 15,
+                131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
+                190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
+                88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
+                77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
+                102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
+                135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
+                5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
+                223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
+                129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
+                251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
+                49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
+                138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180};
+
+        // To remove the need for index wrapping, double the permutation table length
+        private static final short[] perm = new short[512];
+        private static final short[] permMod12 = new short[512];
+
+        static {
+            for (int i = 0; i < 512; i++) {
+                perm[i] = p[i & 255];
+                permMod12[i] = (short) (perm[i] % 12);
+            }
+        }
+
+        // Skewing and unskewing factors for 2, 3, and 4 dimensions
+        private static final float F3 = 1.0f / 3.0f;
+        private static final float G3 = 1.0f / 6.0f;
+
+        // This method is a *lot* faster than using (int)Math.floor(x)
+        private static int fastfloor(float x) {
+            int xi = (int) x;
+            return x < xi ? xi - 1 : xi;
+        }
+
+        private static float dot(Grad g, float x, float y, float z) {
+            return g.x * x + g.y * y + g.z * z;
+        }
+
+        // 3D simplex noise
+        static float noise(float xin, float yin, float zin) {
+            float n0, n1, n2, n3; // Noise contributions from the four corners
+            // Skew the input space to determine which simplex cell we're in
+            float s = (xin + yin + zin) * F3; // Very nice and simple skew factor for 3D
+            int i = fastfloor(xin + s);
+            int j = fastfloor(yin + s);
+            int k = fastfloor(zin + s);
+            float t = (i + j + k) * G3;
+            float X0 = i - t; // Unskew the cell origin back to (x,y,z) space
+            float Y0 = j - t;
+            float Z0 = k - t;
+            float x0 = xin - X0; // The x,y,z distances from the cell origin
+            float y0 = yin - Y0;
+            float z0 = zin - Z0;
+            // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
+            // Determine which simplex we are in.
+            int i1, j1, k1; // Offsets for second corner of simplex in (i,j,k) coords
+            int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
+            if (x0 >= y0) {
+                if (y0 >= z0) {
+                    i1 = 1;
+                    j1 = 0;
+                    k1 = 0;
+                    i2 = 1;
+                    j2 = 1;
+                    k2 = 0;
+                } // X Y Z order
+                else if (x0 >= z0) {
+                    i1 = 1;
+                    j1 = 0;
+                    k1 = 0;
+                    i2 = 1;
+                    j2 = 0;
+                    k2 = 1;
+                } // X Z Y order
+                else {
+                    i1 = 0;
+                    j1 = 0;
+                    k1 = 1;
+                    i2 = 1;
+                    j2 = 0;
+                    k2 = 1;
+                } // Z X Y order
+            } else { // x0<y0
+                if (y0 < z0) {
+                    i1 = 0;
+                    j1 = 0;
+                    k1 = 1;
+                    i2 = 0;
+                    j2 = 1;
+                    k2 = 1;
+                } // Z Y X order
+                else if (x0 < z0) {
+                    i1 = 0;
+                    j1 = 1;
+                    k1 = 0;
+                    i2 = 0;
+                    j2 = 1;
+                    k2 = 1;
+                } // Y Z X order
+                else {
+                    i1 = 0;
+                    j1 = 1;
+                    k1 = 0;
+                    i2 = 1;
+                    j2 = 1;
+                    k2 = 0;
+                } // Y X Z order
+            }
+            // A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
+            // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
+            // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z), where
+            // c = 1/6.
+            float x1 = x0 - i1 + G3; // Offsets for second corner in (x,y,z) coords
+            float y1 = y0 - j1 + G3;
+            float z1 = z0 - k1 + G3;
+            float x2 = x0 - i2 + 2.0f * G3; // Offsets for third corner in (x,y,z) coords
+            float y2 = y0 - j2 + 2.0f * G3;
+            float z2 = z0 - k2 + 2.0f * G3;
+            float x3 = x0 - 1.0f + 3.0f * G3; // Offsets for last corner in (x,y,z) coords
+            float y3 = y0 - 1.0f + 3.0f * G3;
+            float z3 = z0 - 1.0f + 3.0f * G3;
+            // Work out the hashed gradient indices of the four simplex corners
+            int ii = i & 255;
+            int jj = j & 255;
+            int kk = k & 255;
+            int gi0 = permMod12[ii + perm[jj + perm[kk]]];
+            int gi1 = permMod12[ii + i1 + perm[jj + j1 + perm[kk + k1]]];
+            int gi2 = permMod12[ii + i2 + perm[jj + j2 + perm[kk + k2]]];
+            int gi3 = permMod12[ii + 1 + perm[jj + 1 + perm[kk + 1]]];
+            // Calculate the contribution from the four corners
+            float t0 = 0.6f - x0 * x0 - y0 * y0 - z0 * z0;
+            if (t0 < 0) n0 = 0.0f;
+            else {
+                t0 *= t0;
+                n0 = t0 * t0 * dot(grad3[gi0], x0, y0, z0);
+            }
+            float t1 = 0.6f - x1 * x1 - y1 * y1 - z1 * z1;
+            if (t1 < 0) n1 = 0.0f;
+            else {
+                t1 *= t1;
+                n1 = t1 * t1 * dot(grad3[gi1], x1, y1, z1);
+            }
+            float t2 = 0.6f - x2 * x2 - y2 * y2 - z2 * z2;
+            if (t2 < 0) n2 = 0.0f;
+            else {
+                t2 *= t2;
+                n2 = t2 * t2 * dot(grad3[gi2], x2, y2, z2);
+            }
+            float t3 = 0.6f - x3 * x3 - y3 * y3 - z3 * z3;
+            if (t3 < 0) n3 = 0.0f;
+            else {
+                t3 *= t3;
+                n3 = t3 * t3 * dot(grad3[gi3], x3, y3, z3);
+            }
+            // Add contributions from each corner to get the final noise value.
+            // The result is scaled to stay just inside [-1,1]
+            return 32.0f * (n0 + n1 + n2 + n3);
+        }
+
+        // Inner class to speed upp gradient computations
+        // (In Java, array access is a lot slower than member access)
+        private static class Grad {
+            float x, y, z;
+
+            Grad(float x, float y, float z) {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
     }
 }
