0c93c37586bde001e02fc6d8f12aff4d61630ce5
Jack Papel <jackpapel@live.com>
2021-09-19 20:14:22 -0700

    Move things a little more
    Speed things up by only needing to register them once.

--- src/main/java/net/id/aether/client/rendering/AetherColorProviders.java
+++ src/main/java/net/id/aether/client/rendering/AetherColorProviders.java
@@ -6,30 +6,34 @@ import net.id.aether.util.DynamicColorBlock;
 import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.client.color.world.FoliageColors;
 import net.minecraft.client.color.world.GrassColors;
-import net.minecraft.item.BlockItem;
 import net.minecraft.util.registry.Registry;
 
 public class AetherColorProviders {
     public static void initClient() {
         initBlocks();
         initItems();
+        initDynamicColorBlocks();
     }
 
     private static void initBlocks() {
         ColorProviderRegistryImpl.BLOCK.register(((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefaultColor()), AetherBlocks.SKYROOT_LEAVES, AetherBlocks.SKYROOT_LEAF_PILE);
         ColorProviderRegistryImpl.BLOCK.register(((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.getColor(0.5D, 1.0D)), AetherBlocks.AETHER_GRASS_BLOCK, AetherBlocks.AETHER_GRASS, AetherBlocks.AETHER_GRASS_FLOWERING, AetherBlocks.AETHER_TALL_GRASS, AetherBlocks.AETHER_FERN, AetherBlocks.AETHER_BUSH);
         ColorProviderRegistryImpl.BLOCK.register(((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : -1), AetherBlocks.HONEY_NETTLE, AetherBlocks.WEEPING_CLOUDBURST);
-        // ideally we shouldn't go through the entire block registry, but it only runs once anyway.
-        Registry.BLOCK.stream()
-                .filter(block -> block instanceof DynamicColorBlock)
-                .forEach(block -> ColorProviderRegistryImpl.BLOCK.register(((DynamicColorBlock) block).getBlockColorProvider(), block));
     }
 
     private static void initItems() {
         ColorProviderRegistryImpl.ITEM.register(((stack, tintIndex) -> 0xbce632), AetherBlocks.SKYROOT_LEAVES.asItem(), AetherBlocks.SKYROOT_LEAF_PILE.asItem());
         ColorProviderRegistryImpl.ITEM.register(((stack, tintIndex) -> 0xB1FFCB), AetherBlocks.AETHER_GRASS_BLOCK.asItem(), AetherBlocks.AETHER_GRASS.asItem(), AetherBlocks.AETHER_TALL_GRASS.asItem(), AetherBlocks.AETHER_FERN.asItem(), AetherBlocks.AETHER_BUSH.asItem());
-        Registry.ITEM.stream()
-                .filter(item -> item instanceof BlockItem blockItem && blockItem.getBlock() instanceof DynamicColorBlock)
-                .forEach(item -> ColorProviderRegistryImpl.ITEM.register(((DynamicColorBlock) ((BlockItem) item).getBlock()).getBlockItemColorProvider(), item));
+    }
+
+    private static void initDynamicColorBlocks() {
+        // Ideally we shouldn't go through the entire block registry, but it's almost instantaneous anyway
+        Registry.BLOCK.stream()
+                .filter(block -> block instanceof DynamicColorBlock)
+                .forEach(block -> {
+                    DynamicColorBlock dynamic = (DynamicColorBlock) block;
+                    ColorProviderRegistryImpl.BLOCK.register(dynamic.getBlockColorProvider(), block);
+                    ColorProviderRegistryImpl.ITEM.register(dynamic.getBlockItemColorProvider(), block.asItem());
+                }).;
     }
 }
