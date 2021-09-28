14a0f9c3435d4a161fbee51f82123a32fc015974
Jack Papel <jackpapel@live.com>
2021-09-27 17:50:49 -0700

    Persistence docs

--- src/main/java/net/id/aether/effect/condition/Persistence.java
+++ src/main/java/net/id/aether/effect/condition/Persistence.java
@@ -1,24 +1,40 @@
 package net.id.aether.effect.condition;
 
 /**
- * The temporary persistence is what you expect. It is given by things like
- * venom arrows, or whatever, and goes away with time.
- *
- * The chronic persistence is like the temporary persistence, but it doesn't
- * go down with time (in theory). It is reduced by consumables.
- *
- * The constant persistence is given by things like armors and trinkets that
- * apply conditions. It goes away when the armor or trinket is removed.
- *
+ * {@code Persistence} is how much a {@code Condition} persists.
+ * There are three main types: {@link Persistence#TEMPORARY},
+ * {@link Persistence#CHRONIC}, and {@link Persistence#CONSTANT}
+ * <br><br>
  * ~ Jack
+ * @author AzazelTheDemonLord
+ * @see Persistence#TEMPORARY
+ * @see Persistence#CHRONIC
+ * @see Persistence#CONSTANT
  */
 public enum Persistence {
+    /**
+     * This {@code Persistence} is what you'd expect.
+     * It's given by things like projectiles, for example, and goes away with time.
+     */
     TEMPORARY("condition.persistence.temporary"),
+    /**
+     * This is like the {@link Persistence#TEMPORARY} {@code Persistence},
+     * but it doesn't go down with time (in theory). It is reduced by
+     * consumables.
+     */
     CHRONIC("condition.persistence.chronic"),
+    /**
+     * The {@code CONSTANT} {@code Persistence} is given by things like
+     * armors and trinkets that apply conditions. It goes away when the
+     * armor or trinket is removed.
+     */
     CONSTANT("condition.persistence.constant");
 
     public final String translation;
 
+    /**
+     * @param translation The translation key for the {@code Persistence}
+     */
     Persistence(String translation) {
         this.translation = translation;
     }
