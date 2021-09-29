b78f0f979a4c6349f0ea9ec3833d7cf749f62324
Jack Papel <jackpapel@live.com>
2021-09-28 19:05:50 -0700

    ConditionAPI docs

--- src/main/java/net/id/aether/api/ConditionAPI.java
+++ src/main/java/net/id/aether/api/ConditionAPI.java
@@ -12,7 +12,17 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+/**
+ * An API intended to aid use of {@code Condition}s. <br>
+ * <br>
+ * ~ Jack
+ * @author AzazelTheDemonLord
+ */
 public class ConditionAPI {
+    /**
+     * @param type The {@code EntityType} to test
+     * @return A list of all conditions the given entity is not immune to.
+     */
     public static List<Condition> getValidConditions(EntityType<?> type) {
         return AetherRegistries.CONDITION_REGISTRY
                 .stream()
@@ -20,10 +30,20 @@ public class ConditionAPI {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * @param id The unique {@code Identifier} of the {@code Condition}.
+     * @return The {@code Condition} corresponding to the given {@code Identifier}
+     */
     public static Condition getOrThrow(Identifier id) {
         return AetherRegistries.CONDITION_REGISTRY.getOrEmpty(id).orElseThrow((() -> new NoSuchElementException("No ConditionManager found registered for entry: " + id.toString())));
     }
 
+    /**
+     * @param condition The {@code Condition} to test
+     * @param entity The entity to test
+     * @return Whether the given {@code Condition} is currently outwardly
+     * visible on the given entity.
+     */
     public static boolean isVisible(Condition condition, LivingEntity entity) {
         if(!condition.isExempt(entity)) {
             return AetherComponents.CONDITION_MANAGER_KEY.get(entity).getScaledSeverity(condition) >= condition.visThreshold;
@@ -31,10 +51,18 @@ public class ConditionAPI {
         return false;
     }
 
+    /**
+     * @param entity The entity you want to get the {@code ConditionManager} of.
+     * @return The {@code ConditionManager} of the given entity.
+     */
     public static ConditionManager getConditionManager(LivingEntity entity) {
         return AetherComponents.CONDITION_MANAGER_KEY.get(entity);
     }
 
+    /**
+     * Sync's a given entities {@code ConditionManager}.
+     * @param entity The entity whose {@code ConditionManager} you wish to sync.
+     */
     public static void trySync(LivingEntity entity) {
         AetherComponents.CONDITION_MANAGER_KEY.sync(entity);
     }
