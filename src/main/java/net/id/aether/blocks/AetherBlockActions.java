3c3a5547717fd2eae04521a829a853a94cc607bf
Jack Papel <jackpapel@live.com>
2021-10-06 14:17:57 -0700

    Simplify AetherBlockActions
    Static import some common things, such as Actions and the onClient method.

--- src/main/java/net/id/aether/blocks/AetherBlockActions.java
+++ src/main/java/net/id/aether/blocks/AetherBlockActions.java
@@ -6,30 +6,32 @@ import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.fabricmc.fabric.api.registry.TillableBlockRegistry;
 import net.fabricmc.fabric.mixin.lookup.BlockEntityTypeAccessor;
 import net.id.aether.util.RenderUtils;
-import net.id.incubus_core.util.RegistryQueue;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.AbstractSignBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.HoeItem;
 
+import static net.id.incubus_core.util.RegistryQueue.Action;
+import static net.id.incubus_core.util.RegistryQueue.onClient;
+
 class AetherBlockActions {
     protected static final AbstractBlock.ContextPredicate never = (state, view, pos) -> false;
     protected static final AbstractBlock.ContextPredicate always = (state, view, pos) -> true;
 
-    protected static RegistryQueue.Action<Block> flammable(int spread, int burn) { return (id, block) -> FlammableBlockRegistry.getDefaultInstance().add(block, spread, burn);}
-    protected static final RegistryQueue.Action<Block> flammableLog = flammable(5, 5);
-    protected static final RegistryQueue.Action<Block> flammablePlanks = flammable(20, 5);
-    protected static final RegistryQueue.Action<Block> flammableLeaves = flammable(60, 30);
-    protected static final RegistryQueue.Action<Block> flammablePlant = flammable(60, 100);
+    protected static Action<Block> flammable(int spread, int burn) { return (id, block) -> FlammableBlockRegistry.getDefaultInstance().add(block, spread, burn);}
+    protected static final Action<Block> flammableLog = flammable(5, 5);
+    protected static final Action<Block> flammablePlanks = flammable(20, 5);
+    protected static final Action<Block> flammableLeaves = flammable(60, 30);
+    protected static final Action<Block> flammablePlant = flammable(60, 100);
 
-    protected static final RegistryQueue.Action<Block> translucentRenderLayer = RegistryQueue.onClient((id, block) -> RenderUtils.transparentRenderLayer(block));
-    protected static final RegistryQueue.Action<Block> cutoutRenderLayer = RegistryQueue.onClient((id, block) -> RenderUtils.cutoutRenderLayer(block));
-    protected static final RegistryQueue.Action<Block> cutoutMippedRenderLayer = RegistryQueue.onClient((id, block) -> RenderUtils.cutoutMippedRenderLayer(block));
+    protected static final Action<Block> translucentRenderLayer = onClient((id, block) -> RenderUtils.transparentRenderLayer(block));
+    protected static final Action<Block> cutoutRenderLayer = onClient((id, block) -> RenderUtils.cutoutRenderLayer(block));
+    protected static final Action<Block> cutoutMippedRenderLayer = onClient((id, block) -> RenderUtils.cutoutMippedRenderLayer(block));
 
-    protected static final RegistryQueue.Action<AbstractSignBlock> signBlockEntity = (id, block) -> ((BlockEntityTypeAccessor) BlockEntityType.SIGN).getBlocks().add(block);
+    protected static final Action<AbstractSignBlock> signBlockEntity = (id, block) -> ((BlockEntityTypeAccessor) BlockEntityType.SIGN).getBlocks().add(block);
 
-    protected static RegistryQueue.Action<Block> strippedFrom(Block original) { return (id, stripped) -> StrippableBlockRegistry.register(original, stripped);}
-    protected static RegistryQueue.Action<Block> tillable() { return (id, block) -> TillableBlockRegistry.register(block, HoeItem::canTillFarmland, AetherBlocks.AETHER_FARMLAND.getDefaultState());}
-    protected static RegistryQueue.Action<Block> flattenable() { return (id, block) -> FlattenableBlockRegistry.register(block, AetherBlocks.AETHER_DIRT_PATH.getDefaultState());}
+    protected static Action<Block> strippedFrom(Block original) { return (id, stripped) -> StrippableBlockRegistry.register(original, stripped);}
+    protected static Action<Block> tillable() { return (id, block) -> TillableBlockRegistry.register(block, HoeItem::canTillFarmland, AetherBlocks.AETHER_FARMLAND.getDefaultState());}
+    protected static Action<Block> flattenable() { return (id, block) -> FlattenableBlockRegistry.register(block, AetherBlocks.AETHER_DIRT_PATH.getDefaultState());}
 }
