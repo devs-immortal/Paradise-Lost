a19ee4a54cd52379aa69bce59f0c9803d259ab1d
azzy <danielferrerreyes@protonmail.com>
2021-10-31 00:47:20 -0500

    Test

--- src/main/java/net/id/aether/world/feature/AetherConfiguredFeatures.java
+++ src/main/java/net/id/aether/world/feature/AetherConfiguredFeatures.java
@@ -44,6 +44,8 @@ import java.util.Set;
 
 import static net.id.aether.blocks.AetherBlocks.*;
 
+//Test
+
 @SuppressWarnings("unchecked")
 public class AetherConfiguredFeatures extends ConfiguredFeatures {
 
