c6baddc9ee8d3c2afbc3538db5ebc9d52e7e0c90
Jack Papel <jackpapel@live.com>
2021-11-21 13:07:08 -0800

    Don't flip players
    Since they can't be floated, they shouldn't be flipped either.

--- src/main/java/net/id/aether/mixin/client/render/LivingEntityRendererMixin.java
+++ src/main/java/net/id/aether/mixin/client/render/LivingEntityRendererMixin.java
@@ -4,6 +4,7 @@ import net.id.aether.entities.AetherEntityExtensions;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +16,10 @@ public class LivingEntityRendererMixin {
     @Inject(method = "setupTransforms", at = @At("TAIL"))
     private void setupTransforms(LivingEntity entity, MatrixStack matrices, float animationProgress, float bodyYaw, float tickDelta, CallbackInfo ci) {
         if (((AetherEntityExtensions) entity).getFlipped()) {
-            matrices.translate(0.0D, entity.getHeight() + 0.1F, 0.0D);
-            matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0F));
+            if (!(entity instanceof PlayerEntity)) {
+                matrices.translate(0.0D, entity.getHeight() + 0.1F, 0.0D);
+                matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0F));
+            }
         }
     }
 }
