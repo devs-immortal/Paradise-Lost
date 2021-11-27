35c6990fa9719c31db5cad98e738795996353632
Jack Papel <jackpapel@live.com>
2021-11-27 14:51:47 -0800

    Max quicksoil speed limit for boats, for real this time
    The boats' speed limit is *slightly* faster than the normal one, but it now doesn't shoot off.

--- src/main/java/net/id/aether/mixin/entity/EntityMixin.java
+++ src/main/java/net/id/aether/mixin/entity/EntityMixin.java
@@ -7,6 +7,8 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.entity.vehicle.MinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,10 +20,18 @@ public abstract class EntityMixin {
     @Redirect(method = "getVelocityMultiplier", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getVelocityMultiplier()F"), require = 2)
     private float getVelocityMultiplier(Block target) {
         if (target == AetherBlocks.QUICKSOIL || target == AetherBlocks.QUICKSOIL_GLASS || target == AetherBlocks.QUICKSOIL_GLASS_PANE) {
-            double maxSpeed = ((Entity) (Object) this).world.getGameRules().get(AetherGameRules.MAX_QUICKSOIL_SPEED).get();
-            return (float) (1 + Math.max(
-                    (maxSpeed - ((Entity) (Object) this).getVelocity().horizontalLength()) / maxSpeed * 0.102,
-                    0));
+            Entity entity = ((Entity)(Object)this);
+            boolean isVehicle = entity instanceof BoatEntity || entity instanceof MinecartEntity;
+
+            double maxSpeed = entity.world.getGameRules().get(AetherGameRules.MAX_QUICKSOIL_SPEED).get();
+            maxSpeed = isVehicle ? maxSpeed * 0.16D : maxSpeed;
+            float calculatedChange = (float) ((maxSpeed - entity.getVelocity().horizontalLength()) / maxSpeed * 0.102);
+
+            if (isVehicle) {
+                return Math.min(1.0F, 1.0F + calculatedChange);
+            } else {
+                return (1 + Math.max(calculatedChange, 0));
+            }
         }
         return target.getVelocityMultiplier();
     }
