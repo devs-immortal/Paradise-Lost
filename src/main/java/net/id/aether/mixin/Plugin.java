ef8a891e03a6cbc4f840a630958874f6cd237b8c
gudenau <gudenau@users.noreply.github.com>
2021-11-26 20:03:06 -0600

    Attempted satin fix

--- src/main/java/net/id/aether/mixin/Plugin.java
+++ src/main/java/net/id/aether/mixin/Plugin.java
@@ -1,4 +1,4 @@
-package net.id.aether.mixin.devel;
+package net.id.aether.mixin;
 
 import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
@@ -11,6 +11,14 @@ import java.util.Set;
 public final class Plugin implements IMixinConfigPlugin{
     private static final boolean isDevel = FabricLoader.getInstance().isDevelopmentEnvironment();
     
+    private static final class Compat{
+        private static final boolean satin = isLoaded("satin");
+        
+        private static boolean isLoaded(String id){
+            return FabricLoader.getInstance().isModLoaded(id);
+        }
+    }
+    
     @Override
     public void onLoad(String mixinPackage){}
     
@@ -21,6 +29,10 @@ public final class Plugin implements IMixinConfigPlugin{
     
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName){
+        if(Compat.satin && mixinClassName.equals("net.id.aether.mixin.client.render.ShaderIdFixMixin")){
+            return false;
+        }
+        
         if(isDevel){
             return true;
         }
--- src/main/java/net/id/aether/mixin/client/render/ShaderMixin.java
+++ src/main/java/net/id/aether/mixin/client/render/ShaderMixin.java
@@ -11,9 +11,7 @@ import net.minecraft.client.render.Shader;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.resource.ResourceFactory;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -26,7 +24,6 @@ import static org.lwjgl.opengl.GL33.glBindTexture;
 public abstract class ShaderMixin implements ShaderDuck{
     @Shadow public abstract @Nullable GlUniform getUniform(String name);
     
-    @Unique private boolean the_aether$isAetherShader;
     @Unique private GlUniform the_aether$time;
     
     @Unique
@@ -39,59 +36,11 @@ public abstract class ShaderMixin implements ShaderDuck{
         at = @At("TAIL")
     )
     private void init(ResourceFactory factory, String name, VertexFormat format, CallbackInfo ci){
-        if(the_aether$isAetherShader){
+        if(name.contains(Aether.MOD_ID + ':')){
             the_aether$time = getUniform("Time");
         }else{
             the_aether$time = null;
         }
-    }
-    
-    @ModifyArg(
-        method = "<init>",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/util/Identifier;<init>(Ljava/lang/String;)V"
-        ),
-        index = 0
-    )
-    private String init$fixId(String id){
-        if(id.contains(Aether.MOD_ID + ':')){
-            the_aether$isAetherShader = true;
-            if(id.startsWith(Aether.MOD_ID + ':')){
-                return id;
-            }
-            var split = id.split(":", 2);
-            return Aether.MOD_ID + ":shaders/core/" + split[1];
-        }
-        the_aether$isAetherShader = false;
-        return id;
-    }
-    
-    @ModifyVariable(
-        method = "loadProgram",
-        at = @At(
-            value = "LOAD"
-        ),
-        ordinal = 1,
-        slice = @Slice(
-            from = @At(
-                value = "HEAD"
-            ),
-            to = @At(
-                value = "INVOKE",
-                target = "Lnet/minecraft/resource/ResourceFactory;getResource(Lnet/minecraft/util/Identifier;)Lnet/minecraft/resource/Resource;"
-            )
-        )
-    )
-    private static String loadProgram$fixId(String id){
-        if(id.contains(Aether.MOD_ID + ':')){
-            if(id.startsWith(Aether.MOD_ID + ':')){
-                return id;
-            }
-            var split = id.split(":", 2);
-            return Aether.MOD_ID + ":shaders/core/" + split[1];
-        }
-        return id;
     }
     
     //FIXME Figure out a better way to handle this
