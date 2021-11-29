f51f1d92bc2a324bd894fd344429a409f99391ed
gudenau <gudenau@users.noreply.github.com>
2021-11-29 15:51:57 -0600

    Added a data fixer class for future use.

---
+++ src/main/java/net/id/aether/util/AetherDataFixers.java
@@ -0,0 +1,53 @@
+package net.id.aether.util;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//TODO Make this work, for now this is a reference to names that need changing.
+public final class AetherDataFixers{
+    private AetherDataFixers(){}
+    
+    private static final String OLD_NAMESPACE = "the_aether";
+    private static final String NEW_NAMESPACE = "aether";
+    
+    private static final Map<String, String> BLOCKS = createMap(
+        "aether_grass", "grass",
+        "enchanted_aether_grass", "enchanted_grass",
+        "aether_frozen_grass", "frozen_grass",
+        "aether_dirt", "dirt",
+        "coarse_aether_dirt", "coarse_dirt",
+        "aether_farmland", "farmland",
+        "aether_grass_path", "grass_path",
+        "aether_grass_plant", "grass_plant",
+        "aether_grass_flowering", "grass_flowering",
+        "aether_short_grass", "short_grass",
+        "aether_tall_grass", "tall_grass",
+        "aether_fern", "fern",
+        "potted_aether_fern", "potted_fern",
+        "aether_bush", "bush"
+    );
+    
+    private static final Map<String, String> ITEMS = mergeMaps(createMap(
+    
+    ), BLOCKS);
+    
+    private static <T> Map<T, T> createMap(T... values){
+        if((values.length & 1) != 0){
+            throw new IllegalArgumentException("Odd number of values");
+        }
+        Map<T, T> map = new HashMap<>();
+        for(int i = 0; i < values.length; i += 2){
+            map.put(values[i], values[i + 1]);
+        }
+        return Collections.unmodifiableMap(map);
+    }
+    
+    @SafeVarargs
+    private static <K, V> Map<K, V> mergeMaps(Map<K, V>... maps){
+        return Stream.of(maps)
+            .map(Map::entrySet)
+            .flatMap(Collection::stream)
+            .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
