4b049075288bee60cbeb14af9a2358544a9f3e58
CDAGaming <cstack2011@yahoo.com>
2021-11-30 01:04:01 -0600

    Bump version + Blue Swets now evaporate in >1.0f temperature
    - https://github.com/devs-immortal/The-Aether/issues/543

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 - Stopped rooks from spawning. They'll be adjusted for 1.7
 - Began work on datafixers behind-the-scenes
 - Swet jiggle physics have been slightly changed
+- Blue Swets now burn up in the Nether
 
 # Fixes
 
--- src/main/java/net/id/aether/entities/hostile/swet/BlueSwetEntity.java
+++ src/main/java/net/id/aether/entities/hostile/swet/BlueSwetEntity.java
@@ -1,13 +1,31 @@
 package net.id.aether.entities.hostile.swet;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class BlueSwetEntity extends SwetEntity {
     public BlueSwetEntity(EntityType<? extends BlueSwetEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    public void tickMovement() {
+        super.tickMovement();
+        if (!this.world.isClient) {
+            int i = MathHelper.floor(this.getX());
+            int j = MathHelper.floor(this.getY());
+            int k = MathHelper.floor(this.getZ());
+            BlockPos blockPos = new BlockPos(i, 0, k);
+            BlockPos blockPos2 = new BlockPos(i, j, k);
+            if (this.world.getBiome(blockPos).getTemperature(blockPos2) > 1.0f) {
+                this.damage(DamageSource.ON_FIRE, 1.0f);
+            }
+        }
     }
 
     @Override
