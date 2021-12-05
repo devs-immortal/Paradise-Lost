3e7cb92bdee9e2f8553d4bcff3b1c41558bb4528
CDAGaming <cstack2011@yahoo.com>
2021-12-05 14:15:57 -0600

    structure migration

--- src/main/java/net/id/aether/world/feature/structure/OrangeRuinFeature.java
+++ src/main/java/net/id/aether/world/feature/structure/OrangeRuinFeature.java
@@ -2,18 +2,14 @@ package net.id.aether.world.feature.structure;
 
 import com.mojang.serialization.Codec;
 import net.id.aether.world.feature.generator.OrangeRuinGenerator;
-import net.minecraft.structure.*;
+import net.minecraft.structure.StructureGeneratorFactory;
+import net.minecraft.structure.StructurePiecesCollector;
+import net.minecraft.structure.StructurePiecesGenerator;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.DynamicRegistryManager;
-import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.feature.*;
-
-import java.util.Optional;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
 
 public class OrangeRuinFeature extends StructureFeature<DefaultFeatureConfig> {
     public OrangeRuinFeature(Codec<DefaultFeatureConfig> codec) {
