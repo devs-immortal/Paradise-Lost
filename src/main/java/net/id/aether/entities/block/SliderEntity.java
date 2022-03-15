1da96969f12f37d144881538c30d1785b171c771
Jack Papel <jackpapel@live.com>
2022-03-14 20:27:16 -0700

    Add initial slider stuffs + generalize renderer

---
+++ src/main/java/net/id/aether/entities/block/SliderEntity.java
@@ -0,0 +1,25 @@
+package net.id.aether.entities.block;
+
+import net.id.aether.blocks.AetherBlocks;
+import net.id.aether.entities.AetherEntityTypes;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class SliderEntity extends BlockLikeEntity {
+    public SliderEntity(EntityType<? extends BlockLikeEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    public SliderEntity(World world, double x, double y, double z) {
+        super(AetherEntityTypes.SLIDER, world, x, y, z, AetherBlocks.CRACKED_CARVED_STONE.getDefaultState());
+    }
+
+    // Move in a circle. This is obviously not the final behavior.
+    // It's also very jerky for reasons I cannot ascertain.
+    @Override
+    public void postTickMovement() {
+        this.updateVelocity(0.1F, new Vec3d(1D, 0, 0));
+        this.setYaw((this.getYaw() + 6F) % 360.0F);
+    }
+}
