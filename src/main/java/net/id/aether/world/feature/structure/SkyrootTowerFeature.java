2e1f0dbb639beb8bcb384ca7b74f66d522cf9769
24Chrome <m.batt1@outlook.com>
2022-03-17 15:20:59 -0500

    Re-add structures

--- src/main/java/net/id/aether/world/feature/structure/SkyrootTowerFeature.java
+++ src/main/java/net/id/aether/world/feature/structure/SkyrootTowerFeature.java
@@ -3,19 +3,24 @@ package net.id.aether.world.feature.structure;
 import com.mojang.serialization.Codec;
 import net.id.aether.Aether;
 import net.id.aether.world.feature.structure.generator.SkyrootTowerGenerator;
-import net.minecraft.structure.Structure;
-import net.minecraft.structure.StructureGeneratorFactory;
-import net.minecraft.structure.StructurePiecesCollector;
-import net.minecraft.structure.StructurePiecesGenerator;
+import net.minecraft.structure.*;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
+
+import java.util.function.Predicate;
 
 public class SkyrootTowerFeature extends StructureFeature<DefaultFeatureConfig> {
     public SkyrootTowerFeature(Codec<DefaultFeatureConfig> codec) {
@@ -30,16 +35,12 @@ public class SkyrootTowerFeature extends StructureFeature<DefaultFeatureConfig> 
         BlockBox boundingBox = structure.calculateBoundingBox(pos.getStartPos(), blockRotation, pivot, BlockMirror.NONE);
         BlockPos center = boundingBox.getCenter();
         int y = context.chunkGenerator().getHeight(pos.getStartPos().getX() - 4, pos.getStartPos().getZ() - 4, Heightmap.Type.WORLD_SURFACE_WG, context.world());
+        if (y < 0) { // DON'T PLACE ON THE BOTTOM OF THE WORLD
+            return;
+        }
         BlockPos newPos = new BlockPos(pos.getStartPos().getX() - 4, y, pos.getStartPos().getZ() - 4);
         SkyrootTowerGenerator.addPieces(context.structureManager(), collector, blockRotation, newPos);
     }
-
-    // TODO: 1.18 Migrate needed
-    /*@Override
-    @Override
-    protected boolean shouldStartAt(ChunkGenerator chunkGenerator, BiomeSource biomeSource, long worldSeed, ChunkRandom random, ChunkPos pos, Biome biome, ChunkPos chunkPos, DefaultFeatureConfig config, HeightLimitView world) {
-        return chunkGenerator.getHeight(chunkPos.x * 16, chunkPos.z * 16, Heightmap.Type.WORLD_SURFACE_WG, world) >= 7;
-    }*/
 
     @Override
     public GenerationStep.Feature getGenerationStep() {
