2fa2468ef9831f2007e4e7e82a75ff644b3bce1d
24Chrome <m.batt1@outlook.com>
2022-03-17 16:36:55 -0500

    tool refactor and structure placement changes

--- src/main/java/net/id/aether/world/feature/structure/SkyrootTowerFeature.java
+++ src/main/java/net/id/aether/world/feature/structure/SkyrootTowerFeature.java
@@ -27,19 +27,17 @@ public class SkyrootTowerFeature extends StructureFeature<DefaultFeatureConfig> 
         super(codec, StructureGeneratorFactory.simple(StructureGeneratorFactory.checkForBiomeOnTop(Heightmap.Type.WORLD_SURFACE_WG), SkyrootTowerFeature::addPieces));
     }
 
+    private static final int X_OFFSET = 4;
+    private static final int Z_OFFSET = 4;
+
     private static void addPieces(StructurePiecesCollector collector, StructurePiecesGenerator.Context<DefaultFeatureConfig> context) {
-        Structure structure = context.structureManager().getStructureOrBlank(Aether.locate("skyroot_tower"));
-        BlockRotation blockRotation = BlockRotation.NONE;
         ChunkPos pos = context.chunkPos();
-        BlockPos pivot = new BlockPos(structure.getSize().getX() / 2, 0, structure.getSize().getZ() / 2);
-        BlockBox boundingBox = structure.calculateBoundingBox(pos.getStartPos(), blockRotation, pivot, BlockMirror.NONE);
-        BlockPos center = boundingBox.getCenter();
-        int y = context.chunkGenerator().getHeight(pos.getStartPos().getX() - 4, pos.getStartPos().getZ() - 4, Heightmap.Type.WORLD_SURFACE_WG, context.world());
+        int y = context.chunkGenerator().getHeight(pos.getStartPos().getX() - X_OFFSET, pos.getStartPos().getZ() - Z_OFFSET, Heightmap.Type.WORLD_SURFACE_WG, context.world());
         if (y < 0) { // DON'T PLACE ON THE BOTTOM OF THE WORLD
             return;
         }
-        BlockPos newPos = new BlockPos(pos.getStartPos().getX() - 4, y, pos.getStartPos().getZ() - 4);
-        SkyrootTowerGenerator.addPieces(context.structureManager(), collector, blockRotation, newPos);
+        BlockPos newPos = new BlockPos(pos.getStartPos().getX() - X_OFFSET, y, pos.getStartPos().getZ() - Z_OFFSET);
+        SkyrootTowerGenerator.addPieces(context.structureManager(), collector, BlockRotation.NONE, newPos);
     }
 
     @Override
