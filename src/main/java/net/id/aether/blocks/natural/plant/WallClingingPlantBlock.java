061db44e1ea12a572b9c4b8becd56e3b4e210a5c
24Chrome <m.batt1@outlook.com>
2022-03-18 19:27:23 -0500

    Rootcap hitbox

--- src/main/java/net/id/aether/blocks/natural/plant/WallClingingPlantBlock.java
+++ src/main/java/net/id/aether/blocks/natural/plant/WallClingingPlantBlock.java
@@ -1,8 +1,10 @@
 package net.id.aether.blocks.natural.plant;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PlantBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
@@ -10,18 +12,28 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.tag.Tag;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
+
 public class WallClingingPlantBlock extends PlantBlock {
 
+    protected static final Map<Direction, VoxelShape> SHAPES;
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
     private final @Nullable TagKey<Block> clingableBlocks;
 
     public WallClingingPlantBlock(Settings settings, @Nullable TagKey<Block> clingableBlocks) {
         super(settings);
         this.clingableBlocks = clingableBlocks;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPES.get(state.get(FACING));
     }
 
     @Override
@@ -53,5 +65,14 @@ public class WallClingingPlantBlock extends PlantBlock {
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(FACING);
+    }
+
+    static {
+        var builder = ImmutableMap.<Direction, VoxelShape>builder();
+        builder.put(Direction.NORTH, Block.createCuboidShape(0, 4, 0, 16, 12, 6));
+        builder.put(Direction.EAST, Block.createCuboidShape(10, 4, 0, 16, 12, 16));
+        builder.put(Direction.SOUTH, Block.createCuboidShape(0, 4, 10, 16, 12, 16));
+        builder.put(Direction.WEST, Block.createCuboidShape(0, 4, 0, 6, 12, 16));
+        SHAPES = builder.build();
     }
 }
