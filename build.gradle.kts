ff3aa962718eec5f91074e212a111846b8bba841
Solly Watkins <solly@sollyw.com>
2022-06-17 21:38:52 +1000

    skill issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     id("fabric-loom") version "0.12-SNAPSHOT"
     id("com.modrinth.minotaur") version "1.2.1"
     id("de.guntram.mcmod.crowdin-translate") version "1.4+1.18.2"
+    checkstyle
     `maven-publish`
 }
 
