a13fe1a73ed6802045c71b8446c0a50b3501cc35
24Chrome <m.batt1@outlook.com>
2023-01-10 15:44:48 -0600

    new lantern/chain

--- src/main/java/net/id/paradiselost/blocks/decorative/CherineLanternBlock.java
+++ src/main/java/net/id/paradiselost/blocks/decorative/CherineLanternBlock.java
@@ -15,8 +15,8 @@ public class CherineLanternBlock extends LanternBlock {
     protected static final VoxelShape HANGING_SHAPE;
 
     static {
-        STANDING_SHAPE = VoxelShapes.union(Block.createCuboidShape(6.0D, 8.0D, 6.0D, 10.0D, 9.0D, 10.0D), Block.createCuboidShape(5.0D, 1.0D, 5.0D, 11.0D, 7.0D, 11.0D), Block.createCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 1.0D, 12.0D), Block.createCuboidShape(4.0D, 7.0D, 4.0D, 12.0D, 8.0D, 12.0D));
-        HANGING_SHAPE = VoxelShapes.union(Block.createCuboidShape(6.0D, 9.0D, 6.0D, 10.0D, 10.0D, 10.0D), Block.createCuboidShape(5.0D, 2.0D, 5.0D, 11.0D, 8.0D, 11.0D), Block.createCuboidShape(4.0D, 1.0D, 4.0D, 12.0D, 2.0D, 12.0D), Block.createCuboidShape(4.0D, 8.0D, 4.0D, 12.0D, 9.0D, 12.0D));
+        STANDING_SHAPE = VoxelShapes.union(Block.createCuboidShape(6.0D, 8.0D, 6.0D, 10.0D, 9.0D, 10.0D), Block.createCuboidShape(5.0D, 1.0D, 5.0D, 11.0D, 7.0D, 11.0D), Block.createCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 2.0D, 12.0D), Block.createCuboidShape(4.0D, 6.0D, 4.0D, 12.0D, 8.0D, 12.0D));
+        HANGING_SHAPE = VoxelShapes.union(Block.createCuboidShape(6.0D, 9.0D, 6.0D, 10.0D, 10.0D, 10.0D), Block.createCuboidShape(5.0D, 2.0D, 5.0D, 11.0D, 8.0D, 11.0D), Block.createCuboidShape(4.0D, 1.0D, 4.0D, 12.0D, 3.0D, 12.0D), Block.createCuboidShape(4.0D, 7.0D, 4.0D, 12.0D, 9.0D, 12.0D));
     }
 
     public CherineLanternBlock(Settings settings) {
