8470cfa8ab410abc2dc84ec6123d66af7c57e219
24Chrome <m.batt1@outlook.com>
2023-02-01 10:56:44 -0600

    use Minecraft's Simplex Noise implementation #718

--- src/main/java/net/id/paradiselost/blocks/util/DynamicColorBlock.java
+++ src/main/java/net/id/paradiselost/blocks/util/DynamicColorBlock.java
@@ -2,13 +2,14 @@ package net.id.paradiselost.blocks.util;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.id.paradiselost.util.SimplexNoise;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.item.ItemColorProvider;
 import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.noise.SimplexNoiseSampler;
+import net.minecraft.util.math.random.LocalRandom;
 
 public interface DynamicColorBlock {
     @Environment(EnvType.CLIENT)
@@ -33,8 +34,12 @@ public interface DynamicColorBlock {
         return MathHelper.clamp(percent * Math.sqrt((4 + contrast) / (4 + 4 * contrast * percent * percent)) + 0.5, 0, 1);
     }
 
+    SimplexNoiseSampler sampler = new SimplexNoiseSampler(new LocalRandom(0));
+
     // Output range: [0, 1]. clumpSize cannot be 0.
     static double sampleNoise(BlockPos pos, float clumpSize, float offset) {
-        return 0.5 * (1 + SimplexNoise.noise(pos.getX() / clumpSize + offset, pos.getY() / clumpSize + offset, pos.getZ() / clumpSize + offset));
+        return 0.5 * (1 + sampler.sample(pos.getX() / clumpSize + offset, pos.getY() / clumpSize + offset, pos.getZ() / clumpSize + offset));
     }
+
+
 }
