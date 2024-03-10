9afc40f340ac8d931eb005eda9f067590015d603
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-03-10 20:00:06 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val crowdinTranslateVersion: String by properties
 val entityAttributesVersion: String by properties
 val modmenuVersion: String by properties
 val reiVersion: String by properties
-val recipeConfidenceVersion: String by properties
+// val recipeConfidenceVersion: String by properties
 val satinVersion: String by properties
 val clothConfigVersion: String by properties
 
@@ -143,11 +143,11 @@ dependencies {
             version = entityAttributesVersion,
     ).also(::include)
 
-    modImplementation(
+   /* modImplementation(
             group = "net.gudenau.minecraft",
             name = "RecipeConfidence",
             version = recipeConfidenceVersion,
-    ).also(::include)
+    ).also(::include)*/
 
     modImplementation(
             group = "io.github.ladysnake",
