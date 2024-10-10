db8357b2b2c27db37ecfcb6d1058f09cd1648fbb
Maxx <53229958+MBatt1@users.noreply.github.com>
2024-10-10 11:05:03 -0500

    incubator sync fix

--- src/main/java/net/id/paradiselost/blocks/blockentity/IncubatorBlockEntity.java
+++ src/main/java/net/id/paradiselost/blocks/blockentity/IncubatorBlockEntity.java
@@ -8,12 +8,15 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Hand;
-import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
@@ -21,54 +24,77 @@ public class IncubatorBlockEntity extends BlockEntity {
 
     private UUID owner;
     private int hatchTicks = 100;
-    private final DefaultedList<ItemStack> inventory;
+    private ItemStack egg;
 
     public IncubatorBlockEntity(BlockPos pos, BlockState state) {
         super(ParadiseLostBlockEntityTypes.INCUBATOR, pos, state);
-        this.inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
+        this.egg = ItemStack.EMPTY;
     }
 
     public static <T extends BlockEntity> void tickServer(World world, BlockPos pos, BlockState state, T entity) {
         IncubatorBlockEntity incubator = (IncubatorBlockEntity) entity;
-        if (incubator.inventory.get(0).getItem() == ParadiseLostItems.MOA_EGG) {
+        if (incubator.egg.getItem() == ParadiseLostItems.MOA_EGG) {
             incubator.hatchTicks--;
             if (incubator.hatchTicks <= 0) {
                 incubator.hatchTicks = 0;
-                var moa = MoaGenes.getMoaFromEgg(world, incubator.inventory.get(0), incubator.owner);
+                var moa = MoaGenes.getMoaFromEgg(world, incubator.egg, incubator.owner);
                 moa.refreshPositionAndAngles(pos.getX() + 0.25, pos.getY() + 0.65, pos.getZ() + 0.25, world.getRandom().nextFloat() * 360 - 180, 0);
                 world.playSound(null, pos, ParadiseLostSoundEvents.ENTITY_MOA_EGG_HATCH, SoundCategory.BLOCKS, 2F, 0.5F);
                 world.spawnEntity(moa);
-                incubator.inventory.clear();
+                incubator.egg = ItemStack.EMPTY;
+                incubator.markDirty();
             }
         }
     }
 
     public void handleUse(PlayerEntity player, Hand hand, ItemStack handStack) {
         owner = player.getUuid();
-        ItemStack stored = inventory.get(0);
-        inventory.set(0, handStack);
+        ItemStack stored = egg.copy();
+        egg = handStack.copy();
         player.setStackInHand(hand, stored);
         hatchTicks = (int) (12000 / world.getBiome(pos).value().getTemperature());
     }
 
     public boolean hasItem() {
-        return !inventory.isEmpty();
+        return !egg.isEmpty();
     }
 
     public ItemStack getItem() {
-        return inventory.get(0);
+        return egg;
     }
 
     @Override
     public void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.writeNbt(nbt, registryLookup);
+        if (!egg.isEmpty()) {
+            nbt.put("egg", egg.encode(registryLookup));
+        }
         nbt.putInt("hatchTicks", hatchTicks);
     }
 
     @Override
     public void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.readNbt(nbt, registryLookup);
+        if (nbt.contains("egg")) {
+            egg = ItemStack.fromNbt(registryLookup, nbt.get("egg")).get();
+        } else {
+            egg = ItemStack.EMPTY;
+        }
+
         hatchTicks = nbt.getInt("hatchTicks");
+    }
+
+    @Override
+    public NbtCompound toInitialChunkDataNbt(RegistryWrapper.WrapperLookup registryLookup) {
+        NbtCompound nbtCompound = new NbtCompound();
+        this.writeNbt(nbtCompound, registryLookup);
+        return nbtCompound;
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientPlayPacketListener> toUpdatePacket() {
+        return BlockEntityUpdateS2CPacket.create(this);
     }
     
 }
