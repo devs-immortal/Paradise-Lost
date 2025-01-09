b3d40c6464f9b866994b07de2352dea63f4bb610
kalucky0 <kalucki23@gmail.com>
2025-01-09 13:57:34 +0100

    Update loom and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ val customPortalApiVersion: String by properties
 val cardinalComponentsVersion: String by properties
 
 plugins {
-    id("fabric-loom") version "1.7-SNAPSHOT"
-    id("com.modrinth.minotaur") version "1.2.1"
+    id("fabric-loom") version "1.9-SNAPSHOT"
+    id("com.modrinth.minotaur") version "2.8.7"
     id("de.guntram.mcmod.crowdin-translate") version "1.4+1.19"
     `maven-publish`
     checkstyle
