b84d70054d5772f34c02041ef1890662d956da3b
kalucky0 <kalucki23@gmail.com>
2026-06-24 01:28:01 +0200

    fix cloud carvers

--- src/main/java/net/id/paradiselost/world/gen/carver/CloudCarverConfig.java
+++ src/main/java/net/id/paradiselost/world/gen/carver/CloudCarverConfig.java
@@ -3,9 +3,7 @@ package net.id.paradiselost.world.gen.carver;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-import net.id.paradiselost.tag.ParadiseLostBlockTags;
 import net.minecraft.block.Block;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.util.math.floatprovider.FloatProvider;
 import net.minecraft.util.math.intprovider.IntProvider;
@@ -16,28 +14,19 @@ import net.minecraft.world.gen.heightprovider.HeightProvider;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
 public class CloudCarverConfig extends CarverConfig {
-    public static final Codec<CloudCarverConfig> CODEC = RecordCodecBuilder.create((instance) -> {
-        return instance.group(CarverConfig.CONFIG_CODEC.forGetter((carverConfig) -> {
-            return carverConfig;
-        }), FloatProvider.VALUE_CODEC.fieldOf("horizontal_radius_multiplier").forGetter((carverConfig) -> {
-            return carverConfig.horizontalRadiusMultiplier;
-        }), FloatProvider.VALUE_CODEC.fieldOf("vertical_radius_multiplier").forGetter((carverConfig) -> {
-            return carverConfig.verticalRadiusMultiplier;
-        }), BlockStateProvider.TYPE_CODEC.fieldOf("cloud_block").forGetter((cloudCarverConfig -> {
-            return cloudCarverConfig.cloudState;
-        })), FloatProvider.VALUE_CODEC.fieldOf("yaw_multiplier").forGetter((carverConfig) -> {
-            return carverConfig.yawMultiplier;
-        }), FloatProvider.VALUE_CODEC.fieldOf("yaw_pitch_ratio").forGetter((carverConfig) -> {
-            return carverConfig.yawMultiplier;
-        }), IntProvider.VALUE_CODEC.fieldOf("size_multiplier").forGetter((carverConfig) -> {
-            return carverConfig.sizeMultiplier;
-        }), FloatProvider.VALUE_CODEC.fieldOf("max_yaw").forGetter((carverConfig) -> {
-            return carverConfig.maxYaw;
-        }), IntProvider.VALUE_CODEC.fieldOf("engorged_chance").forGetter((carverConfig) -> {
-            return carverConfig.engorgementChance;
-        }), FloatProvider.VALUE_CODEC.fieldOf("width_multiplier").forGetter((carverConfig) -> {
-            return carverConfig.widthMultiplier;
-        })).apply(instance, CloudCarverConfig::new);
+    public static final Codec<CloudCarverConfig> CODEC = RecordCodecBuilder.create(instance -> {
+        return instance.group(
+                CarverConfig.CONFIG_CODEC.forGetter(carverConfig -> carverConfig),
+                FloatProvider.VALUE_CODEC.fieldOf("horizontal_radius_multiplier").forGetter(carverConfig -> carverConfig.horizontalRadiusMultiplier),
+                FloatProvider.VALUE_CODEC.fieldOf("vertical_radius_multiplier").forGetter(carverConfig -> carverConfig.verticalRadiusMultiplier),
+                BlockStateProvider.TYPE_CODEC.fieldOf("cloud_block").forGetter(carverConfig -> carverConfig.cloudState),
+                FloatProvider.VALUE_CODEC.fieldOf("yaw_multiplier").forGetter(carverConfig -> carverConfig.yawMultiplier),
+                FloatProvider.VALUE_CODEC.fieldOf("yaw_pitch_ratio").forGetter(carverConfig -> carverConfig.yawPitchRatio),
+                IntProvider.VALUE_CODEC.fieldOf("size_multiplier").forGetter(carverConfig -> carverConfig.sizeMultiplier),
+                FloatProvider.VALUE_CODEC.fieldOf("max_yaw").forGetter(carverConfig -> carverConfig.maxYaw),
+                IntProvider.VALUE_CODEC.fieldOf("engorged_chance").forGetter(carverConfig -> carverConfig.engorgementChance),
+                FloatProvider.VALUE_CODEC.fieldOf("width_multiplier").forGetter(carverConfig -> carverConfig.widthMultiplier)
+        ).apply(instance, CloudCarverConfig::new);
     });
 
     public final FloatProvider horizontalRadiusMultiplier;
@@ -50,7 +39,7 @@ public class CloudCarverConfig extends CarverConfig {
     public final IntProvider engorgementChance;
     public final BlockStateProvider cloudState;
 
-    public CloudCarverConfig(float probability, HeightProvider y, FloatProvider yScale, YOffset lavaLevel, CarverDebugConfig debugConfig, RegistryEntryList.Named<Block> replaceable, FloatProvider horizontalRadiusMultiplier, FloatProvider verticalRadiusMultiplier, BlockStateProvider cloudState, FloatProvider yawMultiplier, FloatProvider yawPitchRatio, IntProvider sizeMultiplier, FloatProvider maxYaw, IntProvider engorgementChance, FloatProvider widthMultiplier) {
+    public CloudCarverConfig(float probability, HeightProvider y, FloatProvider yScale, YOffset lavaLevel, CarverDebugConfig debugConfig, RegistryEntryList<Block> replaceable, FloatProvider horizontalRadiusMultiplier, FloatProvider verticalRadiusMultiplier, BlockStateProvider cloudState, FloatProvider yawMultiplier, FloatProvider yawPitchRatio, IntProvider sizeMultiplier, FloatProvider maxYaw, IntProvider engorgementChance, FloatProvider widthMultiplier) {
         super(probability, y, yScale, lavaLevel, debugConfig, replaceable);
         this.horizontalRadiusMultiplier = horizontalRadiusMultiplier;
         this.verticalRadiusMultiplier = verticalRadiusMultiplier;
@@ -63,15 +52,7 @@ public class CloudCarverConfig extends CarverConfig {
         this.cloudState = cloudState;
     }
 
-    public CloudCarverConfig(float probability, HeightProvider y, FloatProvider yScale, YOffset lavaLevel, CarverDebugConfig debugConfig, FloatProvider horizontalRadiusMultiplier, FloatProvider verticalRadiusMultiplier, BlockStateProvider cloudState, FloatProvider yawMultiplier, FloatProvider yawPitchRatio, IntProvider sizeMultiplier, FloatProvider maxYaw, IntProvider engorgementChance, FloatProvider widthMultiplier) {
-        this(probability, y, yScale, lavaLevel, debugConfig, Registries.BLOCK.getOrThrow(ParadiseLostBlockTags.CLOUD_CARVER_REPLACEABLES), horizontalRadiusMultiplier, verticalRadiusMultiplier, cloudState, yawMultiplier, yawPitchRatio, sizeMultiplier, maxYaw, engorgementChance, widthMultiplier);
-    }
-
-    public CloudCarverConfig(float probability, HeightProvider y, FloatProvider yScale, YOffset lavaLevel, FloatProvider horizontalRadiusMultiplier, FloatProvider verticalRadiusMultiplier, BlockStateProvider cloudState, FloatProvider yawMultiplier, FloatProvider yawPitchRatio, IntProvider sizeMultiplier, FloatProvider maxYaw, IntProvider engorgementChance, FloatProvider widthMultiplier) {
-        this(probability, y, yScale, lavaLevel, CarverDebugConfig.DEFAULT, horizontalRadiusMultiplier, verticalRadiusMultiplier, cloudState, yawMultiplier, yawPitchRatio, sizeMultiplier, maxYaw, engorgementChance, widthMultiplier);
-    }
-
     public CloudCarverConfig(CarverConfig config, FloatProvider horizontalRadiusMultiplier, FloatProvider verticalRadiusMultiplier, BlockStateProvider cloudState, FloatProvider yawMultiplier, FloatProvider yawPitchRatio, IntProvider sizeMultiplier, FloatProvider maxYaw, IntProvider engorgementChance, FloatProvider widthMultiplier) {
-        this(config.probability, config.y, config.yScale, config.lavaLevel, config.debugConfig, horizontalRadiusMultiplier, verticalRadiusMultiplier, cloudState, yawMultiplier, yawPitchRatio, sizeMultiplier, maxYaw, engorgementChance, widthMultiplier);
+        this(config.probability, config.y, config.yScale, config.lavaLevel, config.debugConfig, config.replaceable, horizontalRadiusMultiplier, verticalRadiusMultiplier, cloudState, yawMultiplier, yawPitchRatio, sizeMultiplier, maxYaw, engorgementChance, widthMultiplier);
     }
 }
